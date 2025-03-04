f5a3d3cdf6dc13da93d6c5b6826350111d54416d
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-04 11:55:01 +0800

    Time: 9 ms (46.97%), Space: 14.9 MB (35.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> search;
+        unordered_map<int, int> numsMap;
         for (int i = 0; i <= nums.size(); i++) {
                 int seeking = target - nums[i];
-                if (search.count(seeking)) {
-                    return vector({i, search[seeking]});
+                if (numsMap.count(seeking)) {
+                    return {i, numsMap[seeking]};
                 }
-                search[nums[i]] = i;
+                numsMap[nums[i]] = i;
         }
         return {-1, -1};
     }
