f3b79453650cd7d77d8a986422d23045ecd7fd8a
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-05 14:30:39 +0800

    Time: 188 ms (5.07%), Space: 83.3 MB (5.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,10 +3,16 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> strsMap;
         vector<vector<string>> ans;
-        for (auto i : strs) {
-            string original = i;
-            sort(i.begin(), i.end());
-            (strsMap[i]).push_back(original);
+        for (auto str : strs) {
+            array<int, 26> charCount = {0};
+            for (auto ch : str) {
+                charCount[ch-'a']++;
+            }
+            string key;
+            for (auto ch : charCount) {
+                key = key + to_string(ch) + "#";
+            }
+            (strsMap[key]).push_back(str);
         }
         for (auto it : strsMap) {
             ans.push_back(it.second);
@@ -15,18 +21,3 @@ public:
     }
 };
 
-/*
-unordered_map<string, vector<string>>
-vector<vector<string>> ans
-for all strs {
-  sorted_str = sort(str[i])
-  map[sorted_str].append(str[i])  
-}
-
-for all map {
-    ans.apppend(map->second)
-}
-
-T: O(n)
-S: O(n)
-*/
