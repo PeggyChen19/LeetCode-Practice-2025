59b11caac024385ffd3a25a5fc9b2f2730db3434
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-08 17:36:03 +0800

    Time: 1 ms (48.91%), Space: 9.8 MB (79.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,12 +1,21 @@
+bool isAlphaNum(char c) {
+    if (('0' <= c && c <= '9') || ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 class Solution {
 public:
     bool isPalindrome(string s) {
         int left = 0;
         int right = s.size() - 1;
         while (left <= right) {
-            if (!isalnum(s[left])) {
+            if (!isAlphaNum(s[left])) {
                 left++;
-            } else if (!isalnum(s[right])) {
+            } else if (!isAlphaNum(s[right])) {
                 right--;
             } else {
                 if (tolower(s[left]) == tolower(s[right])) {
