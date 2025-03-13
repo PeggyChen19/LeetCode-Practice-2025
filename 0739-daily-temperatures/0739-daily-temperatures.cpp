4005a03dbe6de37d6aeb04de20af807c2953c929
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-13 11:22:08 +0800

    Time: 42 ms (9.04%), Space: 108.2 MB (20.89%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> ans;
+        stack<pair<int, int>> st; // value, index
+        for (int i = temperatures.size() - 1; i >= 0; i--) {
+            while (!st.empty() && st.top().first <= temperatures[i]) {
+                st.pop();
+            }
+            if (st.empty()) {
+                ans.push_back(0);
+            } else {
+                ans.push_back((st.top().second - i));
+            }
+            st.push({temperatures[i], i});
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+/*
+we would like to find "the first higher value" on the right
+we can have a stack to record an strict decreasing order from bottom to top, and
+we will build this stack from right to left
+if we iterate to a new value
+    1. pop the top it until meeting a bigger one or reaching the end
+    2. find the ans and then push the new value into stack
+We need to know the both value & index -> record both in stack
+T: O(n), S: O(n)
+*/
