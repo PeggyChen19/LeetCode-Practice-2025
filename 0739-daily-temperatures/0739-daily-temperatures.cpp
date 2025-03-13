ac1c997c0c9d47181995d9144b9c3d9e46e36e17
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-13 11:51:22 +0800

    Time: 33 ms (23.86%), Space: 102.8 MB (91.08%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> ans;
+        vector<int> ans(temperatures.size(), 0);
         stack<pair<int, int>> st; // value, index
         for (int i = temperatures.size() - 1; i >= 0; i--) {
             while (!st.empty() && st.top().first <= temperatures[i]) {
                 st.pop();
             }
-            if (st.empty()) {
-                ans.push_back(0);
-            } else {
-                ans.push_back((st.top().second - i));
+            if (!st.empty()) {
+                ans[i] = st.top().second - i;
             }
             st.push({temperatures[i], i});
         }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
