1662d3960486828e2a896142a28b2a9fd8114993
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-19 16:57:18 +0800

    Time: 27 ms (19.1%), Space: 23.4 MB (64.75%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,44 +11,22 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        queue<TreeNode*> tree_queue;
-        tree_queue.push(root);
-        while (!tree_queue.empty()) {
-            TreeNode* node = tree_queue.front();
-            tree_queue.pop();
-            if (!node) {
-                continue;
+        while (root) {
+            if (p->val < root->val && q->val < root->val) { // go left
+                root = root->left;
+            } else if (p->val > root->val && q->val > root->val) { // go right
+                root = root->right;
+            } else { // the root separate target nodes or the root is one of the target nodes
+                return root;
             }
-            if (isTargetNode(node->left, p, q) && isTargetNode(node->right, p, q)) {
-                return node;
-            } else if (isTargetNode(node->left, p, q))) {
-                return node->left;
-            } else if (isTargetNode(node->right, p, q)) {
-                return node->right;
-            }
-            tree_queue.push(node->left);
-            tree_queue.push(node->right);
         }
         return nullptr;
-    }
-private:
-    bool isTargetNode(TreeNode* node, TreeNode* p, TreeNode* q) {
-        if (!node) {
-            return false;
-        }
-        return (node->val == p->val || node->val == q->val);
     }
 };
 
 /*
-The number of nodes >= 2
-All Node.val are unique.
-p != q, p and q will exist in the BST.
-
-BFS: go into the tree level by level
-to check if the descendants of the current node (including itself) contain "at least one" of the target or not
-    yes: return the current node
-    no: keep going to the lower level
-
-We want to find the lowest node, so the limit will be the highest node -> return when find the higher node
+we can utilize the characteristics of BST
+we can keep going to the subtree which contains both target node
+if we can't find this subtree, it means that the current node seperates the two target nodes OR 
+the current node is one of the target nodes -> current node will be the LCA
 */
