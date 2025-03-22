aa9a34f300f7a32900c0b11f8e336f0787798cac
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-23 00:56:44 +0800

    Time: 4 ms (35.15%), Space: 12.2 MB (29.08%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -26,35 +26,31 @@ public:
             return nullptr;
         }
         unordered_map<Node*, Node*> copies; // original: copied
-        queue<Node*> q;
-        q.push(node);
         Node* copied_node = new Node(node->val);
         copies[node] = copied_node;
-        while (!q.empty()) {
-            Node* cur = q.front();
-            q.pop();
-            for (Node* neighbor : cur->neighbors) {
-                if (!copies.count(neighbor)) { // copied neighbor non-existed
-                    Node* copied_neighbor = new Node(neighbor->val);
-                    copies[neighbor] = copied_neighbor;
-                    q.push(neighbor);
-                }
-                copies[cur]->neighbors.push_back(copies[neighbor]);
-            }
-        }
+        dfs(node, copies);
         return copied_node;
+    }
+private:
+    void dfs(Node* node, unordered_map<Node*, Node*>& copies) {
+        for (auto neighbor : node->neighbors) {
+            if (!copies.count(neighbor)) { // copy non-existed
+                Node* copied_neighbor = new Node(neighbor->val);
+                copies[neighbor] = copied_neighbor;
+                dfs(neighbor, copies); // go into the neighbor
+            }
+            (copies[node]->neighbors).push_back(copies[neighbor]);
+        }
     }
 };
 /*
-BFS go through all nodes
-push root
-create copied root
-while !empty
-    got the front (cur)
+DFS
+unorderd_map copies // original:copied
+created copied root
+dfs
     go through all neighbors
-        if neighbors not exists
-            create with val
-        record the neighbors of cur node
-    push all neighbors
-
+        if copied neighbor non-exist
+            create new one
+            dfs(neighbor)
+        push_back copied_neighbor
 */
