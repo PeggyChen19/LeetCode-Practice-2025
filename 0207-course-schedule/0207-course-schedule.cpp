7051a6bba2dbfa9195f8e5758fb31a976a612fc8
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-23 20:26:24 +0800

    Time: 12 ms (19.18%), Space: 20.2 MB (34.74%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,33 +2,39 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adjacent_list(numCourses);
-        vector<int> indegree(numCourses, 0);
         // build graph
         for (auto pre : prerequisites) {
             adjacent_list[pre[1]].push_back(pre[0]);
-            indegree[pre[0]]++;
         }
-        // check cycle using BFS
-        queue<int> q;
-        int topological_count = 0;
-        for(int i = 0; i < indegree.size(); i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
+        // check cycle using DFS
+        vector<bool> visited(numCourses, false);
+        vector<bool> same_path(numCourses, false);
+        for (int i = 0; i < numCourses; i++) {
+            if(dfs_cycle_detect(adjacent_list, i, visited, same_path)) {
+                return false; // has cycle -> can't finish course
             }
         }
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            topological_count++;
-            for (int neighbor : adjacent_list[cur]) {
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    q.push(neighbor);
-                }
-            }
-        }
-        return topological_count == numCourses;
+        return true;
     }
+private:
+    bool dfs_cycle_detect(vector<vector<int>>& adjacent_list, int i, vector<bool>& visited, vector<bool>& same_path) {
+        if (same_path[i]) {
+            return true; // cycle
+        }
+        if (visited[i]) {
+            return false; // ignore this visited node
+        }
+        visited[i] = true;
+        same_path[i] = true;
+        for (int n = 0; n < adjacent_list[i].size(); n++) {
+            if (dfs_cycle_detect(adjacent_list, adjacent_list[i][n], visited, same_path)) {
+                return true;
+            }
+        }
+        same_path[i] = false;
+        return false;
+    }
+
 };
 
 /*
