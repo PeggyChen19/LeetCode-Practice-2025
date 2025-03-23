d6a9511a171f09b6d04fd93e3fd8c02c64187383
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-23 20:34:04 +0800

    Time: 10 ms (29.73%), Space: 20.2 MB (34.74%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -8,33 +8,32 @@ public:
         }
         // check cycle using DFS
         vector<bool> visited(numCourses, false);
-        vector<bool> same_path(numCourses, false);
+        vector<bool> on_path(numCourses, false);
         for (int i = 0; i < numCourses; i++) {
-            if(dfs_cycle_detect(adjacent_list, i, visited, same_path)) {
+            if(dfs_cycle_detect(adjacent_list, i, visited, on_path)) {
                 return false; // has cycle -> can't finish course
             }
         }
         return true;
     }
 private:
-    bool dfs_cycle_detect(vector<vector<int>>& adjacent_list, int i, vector<bool>& visited, vector<bool>& same_path) {
-        if (same_path[i]) {
+    bool dfs_cycle_detect(const vector<vector<int>>& adjacent_list, int i, vector<bool>& visited, vector<bool>& on_path) {
+        if (on_path[i]) {
             return true; // cycle
         }
         if (visited[i]) {
             return false; // ignore this visited node
         }
         visited[i] = true;
-        same_path[i] = true;
-        for (int n = 0; n < adjacent_list[i].size(); n++) {
-            if (dfs_cycle_detect(adjacent_list, adjacent_list[i][n], visited, same_path)) {
+        on_path[i] = true;
+        for (int neighbor : adjacent_list[i]) {
+            if (dfs_cycle_detect(adjacent_list, neighbor, visited, on_path)) {
                 return true;
             }
         }
-        same_path[i] = false;
+        on_path[i] = false;
         return false;
     }
-
 };
 
 /*
