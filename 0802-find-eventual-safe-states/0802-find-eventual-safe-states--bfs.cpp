2ca5f09116d2453ff84a82059983871bad82629f
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-24 00:28:37 +0800

    Time: 51 ms (39.53%), Space: 66.2 MB (42.1%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states--bfs.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int num_node = graph.size();
+        vector<bool> tmp_ans(num_node, false);
+        vector<int> ans;
+        vector<vector<int>> reverse_graph(num_node);
+        vector<int> indegree(num_node, 0);
+        for (int i = 0; i < num_node; i++) {
+            for (int n : graph[i]) {
+                reverse_graph[n].push_back(i);
+                indegree[i]++;
+            }
+        }
+        queue<int> q;
+        for (int i = 0; i < num_node; i++) {
+            if (indegree[i] == 0) { // terminate nodes
+                q.push(i);
+            }
+        }
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            tmp_ans[cur] = true;
+            for (int n : reverse_graph[cur]) {
+                indegree[n]--;
+                if (indegree[n] == 0) {
+                    q.push(n);
+                }
+            }
+        }
+        for (int i = 0; i < num_node; i++) {
+            if (tmp_ans[i]) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
+/*
+if the node is in a cycle -> not a safe node
+but for a node starts from a node in cycle but reaches terminate node can be a safe node
+use BFS:
+find the nodes which no outcoming node -> terminate node
+push into queue
+while
+    mark front node as safe and remove its incoming edge
+    push new nodes which have not outcoming node
+like topological sort reversed version
+
+          4 -> 5
+          |
+0 -> 1 -> 2 -> 3
+     ^         |
+     |---------|
+
+*/
