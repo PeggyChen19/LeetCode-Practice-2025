febaf6f67cd6447f5ba0ef546e24f8465d47cfa8
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-23 15:55:58 +0800

    Time: 3 ms (89.27%), Space: 22.1 MB (88.13%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    int m;
+    int n;
+    const vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        m = heights.size();
+        n = heights[0].size();
+        vector<vector<bool>> p_ocean(m, vector<bool>(n, false));
+        vector<vector<bool>> a_ocean(m, vector<bool>(n, false));
+        vector<vector<int>> ans;
+        // dfs from margin cells
+        for (int i = 0; i < m; i++) {
+            dfs(heights, i, 0, p_ocean);
+            dfs(heights, i, n - 1, a_ocean);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(heights, 0, j, p_ocean);
+            dfs(heights, m - 1, j, a_ocean);
+        }
+        // find the final ans
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (p_ocean[i][j] && a_ocean[i][j]) {
+                    ans.push_back({i, j});
+                }
+            }
+        }
+        return ans;
+    }
+private:
+    void dfs(vector<vector<int>>& heights, int i, int j, vector<vector<bool>>& visited) {
+        if (visited[i][j]) {
+            return;
+        }
+        visited[i][j] = true;
+        for (auto [dx, dy] : directions) {
+            int x = i + dx;
+            int y = j + dy;
+            if (x >= 0 && x < m && y >= 0 && y < n && heights[x][y] >= heights[i][j]) {
+                dfs(heights, x, y, visited);
+            }
+        }
+    }
+};
+
+/*
+vector<vector<bool>> p
+vector<vector<bool>> a
+go through all cells
+    use dfs to traverse (if the height of neighbor <= cur, go there)
+    until it reaches both ocean, go back to update destination
+*/
