99da6d99b56a896c5e8db630aeb1ca71eaed3d0b
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-24 14:31:12 +0800

    Time: 0 ms (100%), Space: 8.2 MB (83.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         vector<int> record(n, 1);
-        int left = 1;
         for (int i = 1; i < m; i++) {
-            left = 1;
             for (int j = 1; j < n; j++) {
-                left = record[j] + left;
-                record[j] = left;
+                record[j] = record[j] + record[j - 1]; // cell[i][j] = cell[i-1][j] + cell[i][j-1]
             }
         }
         return record[n - 1];
@@ -20,8 +17,10 @@ public:
 3. f(0, i) = f(i, 0) = 1
 4. originally, we need m*n lookup table
     however, we can improve the space usage
-    after finishing calculate row i, we can drop row i-1 and only keep row i for row i+1
+    after finishing calculate row i, we can drop row i-1 (won't use it anymore) and only keep row i for row i+1
     and for column, we only need to record the j-1 (not all previous left columns)
-    -> we only need to record one row and one left
+    -> we only need to record one row and one left value
+    -> 走到 cell[i][j] 之前，record[j] 紀錄的是 cell[i-1][j] 的紀錄，走過之後，更新成 cell[i][j]
+       值得注意的是新更新的 cell[i][j] 剛好是 cell[i][j+1] 需要的 left，所以我們也不需要額外存 left，直接用前一個的 record[j-1] 即可
 5. use double for loop to implement
 */
