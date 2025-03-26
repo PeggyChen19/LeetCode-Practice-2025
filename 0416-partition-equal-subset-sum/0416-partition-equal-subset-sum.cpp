352a6b92a5b272173a5aea16c3e6f22bfc45bf45
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-26 17:28:48 +0800

    Time: 91 ms (74.62%), Space: 13.2 MB (98.82%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return false;
+        }
+        int half_sum = findHalfSum(nums);
+        if (half_sum == -1) { // sum is odd number
+            return false;
+        }
+        vector<bool> cur_sum(half_sum, false);
+        cur_sum[0] = true;
+        if (nums[0] < half_sum) {
+            cur_sum[nums[0]] = true;
+        } else {
+            return false;
+        }
+        for (int i = 1; i < nums.size(); i++) { // start from 1 because 0 is initialized
+            for (int j = half_sum - 1; j >= nums[i]; j--) { // do it backward to avoid cover original content, and prevent index to be negative
+                cur_sum[j] = cur_sum[j - nums[i]] || cur_sum[j];
+            }
+        }
+        return cur_sum[half_sum - 1];
+    }
+private:
+    int findHalfSum(vector<int>& nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+        if ((sum % 2) == 0) {
+            return sum / 2 + 1; // 0 ~ 1/2*sum(nums)
+        } else { // odd number -> cannot be divided
+            return -1;
+        }
+    }
+};
+/*
+We want to choose some nums which sums to 1/2 sum(nums)
+1. State & Subproblem
+Sub-solution: if index 0~i can sum up to cur_sum or not 
+State: index, cur_sum (0~1/2sum(nums))
+2. Decision
+Choose it or not
+3. State Transition Function
+dp(i, sum) = dp(i-1, sum - nums[i]) || dp(i-1, sum)
+              ^ choose                  ^ not choose
+4. Base Case
+dp(0, nums[0]) = true
+dp(0, 0) = true
+5. Computational Sequence (bottom-up / top-down)
+bottom-up: use double for loop to go through all nums & all sum
+6. Memorization - build a lookup table
+build an 1D vector to record sum 0~1/2sum(nums)
+we don't need to record every i because we can only record the last one only
+*/
