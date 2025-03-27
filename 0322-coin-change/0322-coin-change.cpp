68c99d0be73d50946d48f701bae6189efb9ade74
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-27 14:49:17 +0800

    Time: 13 ms (94.75%), Space: 18.2 MB (57.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,39 +4,35 @@ class Solution {
 			if (amount == 0) {
 				return 0;
 			}
-			vector<int> dp(amount + 1, INT_MAX); // 0~amount 
+            sort(coins.rbegin(), coins.rend());
+            int Max = amount + 1;
+			vector<int> dp(amount + 1, Max); // 0~amount
 			dp[0] = 0;
 			for (int i = 0; i < coins.size(); i++) {
 				for (int j = coins[i]; j <= amount; j++) { // j-coins[i] >= 0
-					if (dp[j - coins[i]] != INT_MAX) { // exist solution
-						dp[j] = min(dp[j], dp[j - coins[i]] + 1);
-					}
+					dp[j] = min(dp[j], dp[j - coins[i]] + 1);
 				}
 			}
-            if (dp[amount] == INT_MAX) {
-                return -1;
-            } else {
-                return dp[amount];
-            }
+            return (dp[amount] >= Max) ? -1 : dp[amount];
 		} 
 	};
-	/*
-	1. State & Subproblem
-	state: index, cur_sum
-	subproblem: the fewest number of coins to sum up to cur_sum from index 0~i
-	2. Decision
-	chose 0~infinite coins
-	3. State Transition Function
-	w(i, s) = min (w(i-1, s)    ,   w(i, s-coins[i]) + 1)
-				   ^ not choose     ^ choose one more this coin
-	// if w(i, s-coint[i] == -1), w(i, s) == w(i-1, s)
-	4. Base Case
-	w(-1, 0) = 0
-	w(-1, 1~amount) = -1
-	5. Computational Sequence (bottom-up / top-down)
-	bottom-up: double for loop: index & amount
-	6. Memorization - build a lookup table
-	dp[index][amount] table
-	-> optimize to dp[amount] because we only need i-1 & i
-	update dp[amount] from head
-	*/
+/*
+1. State & Subproblem
+state: index, cur_sum
+subproblem: the fewest number of coins to sum up to cur_sum from index 0~i
+2. Decision
+chose 0~infinite coins
+3. State Transition Function
+w(i, s) = min (w(i-1, s)    ,   w(i, s-coins[i]) + 1)
+                ^ not choose     ^ choose one more this coin
+// if w(i, s-coint[i] == -1), w(i, s) == w(i-1, s)
+4. Base Case
+w(-1, 0) = 0
+w(-1, 1~amount) = -1
+5. Computational Sequence (bottom-up / top-down)
+bottom-up: double for loop: index & amount
+6. Memorization - build a lookup table
+dp[index][amount] table
+-> optimize to dp[amount] because we only need i-1 & i
+update dp[amount] from head
+*/
