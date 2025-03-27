86d46d2b145dd078d2fefca5d24becaaa7feadea
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-27 14:50:10 +0800

    Time: 14 ms (93.58%), Space: 18.2 MB (57.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,21 +1,21 @@
 class Solution {
 	public:
-		int coinChange(vector<int>& coins, int amount) {
-			if (amount == 0) {
-				return 0;
-			}
-            sort(coins.rbegin(), coins.rend());
-            int Max = amount + 1;
-			vector<int> dp(amount + 1, Max); // 0~amount
-			dp[0] = 0;
-			for (int i = 0; i < coins.size(); i++) {
-				for (int j = coins[i]; j <= amount; j++) { // j-coins[i] >= 0
-					dp[j] = min(dp[j], dp[j - coins[i]] + 1);
-				}
-			}
-            return (dp[amount] >= Max) ? -1 : dp[amount];
-		} 
-	};
+    int coinChange(vector<int>& coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        sort(coins.rbegin(), coins.rend());
+        int Max = amount + 1;
+        vector<int> dp(amount + 1, Max); // 0~amount
+        dp[0] = 0;
+        for (int i = 0; i < coins.size(); i++) {
+            for (int j = coins[i]; j <= amount; j++) { // j-coins[i] >= 0
+                dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+            }
+        }
+        return (dp[amount] >= Max) ? -1 : dp[amount];
+    } 
+};
 /*
 1. State & Subproblem
 state: index, cur_sum
@@ -25,7 +25,6 @@ chose 0~infinite coins
 3. State Transition Function
 w(i, s) = min (w(i-1, s)    ,   w(i, s-coins[i]) + 1)
                 ^ not choose     ^ choose one more this coin
-// if w(i, s-coint[i] == -1), w(i, s) == w(i-1, s)
 4. Base Case
 w(-1, 0) = 0
 w(-1, 1~amount) = -1
