806038c89ef2a4f2baacda94aa9627d022f2d780
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-03-30 15:05:46 +0800

    Time: 0 ms (100%), Space: 16.6 MB (94.08%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,44 +3,40 @@ private:
     vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size(), ans = -1;
+        int m = grid.size(), n = grid[0].size();
+        int fresh_count = 0, ans = -1;
         queue<pair<int, int>> q;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) { // rotten -> starting point
+                if (grid[i][j] == 1) {
+                    fresh_count++;
+                } else if (grid[i][j] == 2) { // rotten -> starting point
                     q.push({i, j});
                 }
             }
         }
-        q.push({-1, -1}); // separator
+        if (fresh_count == 0) {
+            return 0;
+        }
         while (!q.empty()) {
-            int i = q.front().first;
-            int j = q.front().second;
-            q.pop();
-            if (i == -1 && j == -1) { // separator
-                if (!q.empty()) { // not the last one
-                    q.push({-1, -1});
-                }
-                ans++;
-                continue;
-            }
-            for (auto [dx, dy] : directions) {
-                int x = i + dx;
-                int y = j + dy;
-                if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) { // fresh
-                    grid[x][y] = 2;
-                    q.push({x, y});
+            int layer_size = q.size();
+            for (int k = 0; k < layer_size; k++) {
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
+                for (auto [dx, dy] : directions) {
+                    int x = i + dx;
+                    int y = j + dy;
+                    if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) { // fresh
+                        grid[x][y] = 2;
+                        fresh_count--;
+                        q.push({x, y});
+                    }
                 }
             }
+            ans++;
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    return -1;
-                }
-            }
-        }
-        return ans;
+        return (fresh_count > 0)? -1 : ans;
     }
 };
 /*
@@ -53,12 +49,12 @@ use a queue (everything in the queue will be rotten)
 queue bfs
 for all rotten
     push into queue
-add a separator
 while q is not empty
-    pop top
-    for all fresh neighbors of top
-        change it to rotten
-        add to queue
-    if encounter separator -> minutes++
+    for all cells in this layer
+        pop top
+        for all fresh neighbors of top
+            change it to rotten
+            add to queue
+    ans++
 check if there has fresh oranges
 */
