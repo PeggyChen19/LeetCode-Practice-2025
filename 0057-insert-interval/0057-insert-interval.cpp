a5803eda00216e2c4a0463e6d3bad1f285fc18ff
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-07 18:06:20 +0800

    Time: 2 ms (35.3%), Space: 21.6 MB (94.03%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        if (intervals.size() == 0) {
-            return {newInterval};
-        }
         vector<vector<int>> ans;
         int i = 0;
         int n = intervals.size();
@@ -31,8 +28,14 @@ New: [4, 5] -> [1, 3], [4, 5], [7, 9]
 New: [2, 5] -> [1, 5], [7, 9]
 New: [2, 10] -> [1, 10]
 
+每個 interval 一定只屬於以下三類之一
+1. 在 new 前，完全沒重疊 -> 直接放入 ans
+2. 處理重疊 -> 開頭取 min，結尾取 max
+3. 在 new 後，完全沒重疊的 -> 直接放入 ans
+比較困難的點是怎麼去判斷，第一種是 i.end < new.start 很明顯，所以第一種結束的時候 i.end >= new.start，所以這兩個變數就不用再考慮了，接下來直接去想怎麼樣才會「重疊」很不直覺，所以可以先去想在 new 後的要怎麼樣才會「不重疊」，得到 new.end < i.start，所以反過來就會是重疊的條件
+-> insight：先去想「不重疊」的條件，再去反推「重疊」的條件，不重疊又分為在前面或在後面
+
 For intervals before new (i.end < new.start) -> insert to ans directly
 For intervals overlap new (new.end >= i.start) -> merge
 For intervals after new (new.end < i.start) -> insert to ans directly
-
 */
