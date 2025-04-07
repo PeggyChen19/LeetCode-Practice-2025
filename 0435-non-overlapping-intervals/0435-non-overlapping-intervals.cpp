5c898ccfdeeb913a9d550054e76ae0b0dc4b49d6
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-08 00:14:38 +0800

    Time: 43 ms (73%), Space: 94.1 MB (40.67%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,18 +1,21 @@
 class Solution {
+private:
+    static bool cmp_end(vector<int>& a, vector<int>& b) {
+        return a[1] < b[1];
+    }
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if (intervals.empty()) {
             return 0;
         }
-        sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(), intervals.end(), cmp_end);
         int ans = 0;
         int pre_interval_end = intervals[0][1];
         for (int i = 1; i < intervals.size(); i++) {
             if (intervals[i][0] < pre_interval_end) { // overlapping
-                ans++;
-                pre_interval_end = min(intervals[i][1], pre_interval_end);
+                ans++; // delete cur -> don't need to update pre
             } else {
-                pre_interval_end = intervals[i][1];
+                pre_interval_end = intervals[i][1]; // update pre
             }
         }
         return ans;
@@ -20,16 +23,17 @@ public:
 };
 
 /*
-sort intervals
-create int ans = 0
-create vector pre_interval = intervals[0]
-go through all intervals except the first one
-    if prev & cur overlap
-        ans++
-        update prev to the one with smaller end
-    else
-        pre = cur
-
 [pre_interval]
         [cur_interval]
+-
+sort intervals by end
+create int ans = 0
+create int pre_interval_end = intervals[0][1]
+go through all intervals except the first one
+    if prev & cur overlap
+        ans++ // delete cur
+    else
+        pre = cur // update pre
+-
+Time: O(nlogn), Space: O(1)
 */
