69c940975e539f47fdcb89d64506bc03550766e0
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-07 18:54:35 +0800

    Time: 7 ms (43.66%), Space: 23.9 MB (35.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        vector<vector<int>> ans;
+        if (intervals.empty()) {
+            return intervals;
+        }
         sort(intervals.begin(), intervals.end());
-        for (int i = 0; i < n - 1; i++) {
-            if (intervals[i][1] < intervals[i+1][0]) { // non-overlapping
+        vector<vector<int>> ans = {intervals[0]};
+        for (int i = 1; i < intervals.size(); i++) {
+            if (ans.back()[1] < intervals[i][0]) { // non-overlapping
                 ans.push_back(intervals[i]);
-            } else { // overlapping -> store the merged one to i+1
-                intervals[i+1][0] = min(intervals[i][0], intervals[i+1][0]);
-                intervals[i+1][1] = max(intervals[i][1], intervals[i+1][1]);
+            } else { // overlapping -> update ans.back()[1]
+                // don't need to update start because intervals are sorted
+                ans.back()[1] = max(intervals[i][1], ans.back()[1]);
             }
         }
-        ans.push_back(intervals[n-1]);
         return ans;
     }
 };
