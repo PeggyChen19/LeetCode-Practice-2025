b0e6cf8651d8d14811828ec81a0ea564a2b7dc6f
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-08 11:20:55 +0800

    Time: 0 ms (100%), Space: 71.6 MB (81.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -11,7 +11,7 @@ public:
     }
 };
 /*
-State: i (end of the subarray), max_sum
+State: i (end of the subarray), cur_max_sum
 Subproblem: the max sum end with i
 這邊不要把 start point 當作 state，因為決定 start point 的時候還不能確定 max_sum，這樣就沒意義了
 通常要 memorize 的東西就是大問題在問的東西
@@ -25,5 +25,5 @@ ans = max(ans, dp[i])
 要注意這邊 dp[size-1] 不一定會是答案，因為他的定義是以 i 為結尾的最大可能，但最好的 subarray 不一定以 size-1 結尾，所以要用 ans 記錄目前最好的
 Implement:Bottom-up
 Memorization:
-Two vars: prev, ans
+Two vars: cur_max, global_max
 */
