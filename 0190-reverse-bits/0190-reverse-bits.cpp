e4b1c484916a62955c27494d8693bb4d38494d6c
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-13 02:29:48 +0800

    Time: 0 ms (100%), Space: 7.6 MB (98.8%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,32 +1,19 @@
 class Solution {
-private:
-    vector<int> look_up = vector<int>(256, -1);
-    uint8_t reverseByte(uint8_t n) {
-        uint8_t modified_n = n;
-        if (look_up[n] == -1) {
-            uint8_t r = 0;
-            for (int i = 0; i < 8; i++) {
-                r = (r << 1) + (modified_n & 1);
-                modified_n = modified_n >> 1;
-            }
-            look_up[n] = r;
-        }
-        return look_up[n];
-    }
 public:
-    uint32_t reverseBits(uint32_t n) {
-        uint32_t r = 0;
-        for (int i = 0; i < 4; i++) {
-            uint8_t byte = (n >> 8*i) & 0b11111111;
-            r = (r << 8) + reverseByte(byte);
-        }
-        return r;
+    uint32_t reverseBits(uint32_t n) {  
+    uint32_t r = 0;
+    for (int i = 0; i < 32; i++) {
+        r = (r << 1) + (n & 1);
+        n = n >> 1;
+    }
+    return r;
     }
 };
 /*
+n=100111
+r=111001
+
 get the right most bit of n: right_most
 left shit r and add right_most to the r
 right shift n
-
-建立 look up table，8 bits 為一組，每次呼叫的時候都把 32 bits 拆成 4 組 8 bits，就可以查表，省去重複計算
 */
