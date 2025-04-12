9eee3271f843e0c62dc1eae98163ae0eb02ae6d9
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-12 23:03:15 +0800

    Time: 0 ms (100%), Space: 8.1 MB (80.35%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,10 +3,8 @@ public:
     int hammingWeight(int n) {
         int ans = 0;
         while (n != 0) {
-            if (n & 1) {
-                ans++;
-            }
-            n = n >> 1;
+            ans++;
+            n = n & (n - 1);
         }
         return ans;
     }
