972f1a37b758a3733c983d7420fa5edf3156ee44
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-19 18:21:43 +0800

    Time: 3 ms (63.57%), Space: 23.2 MB (60.32%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,51 +1,23 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int s = board.size();
-        for (int i = 0; i < s; i++) { // each row
-            if (!validate(i, i, 0, s - 1, board)) {
-                return false;
-            }
-            if (!validate(0, s-1, i, i, board)) { // each col
-                return false;
-            }
-        }
-        for (int i = 0; i < s; i+=3) {
-            for (int j = 0; j < s; j+=3) {
-                if (!validate(i, i+2, j, j+2, board)) {
+        int s = board.size(); // 9 in this problem
+        vector<vector<bool>> row(s, vector<bool>(9, false));
+        vector<vector<bool>> col(s, vector<bool>(9, false));
+        vector<vector<bool>> box(s, vector<bool>(9, false));
+        for (int i = 0; i < s; i++) {
+            for (int j = 0; j < s; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                int box_ind = (i/3) * 3 + (j/3);
+                int num = board[i][j] - '1';
+                if (row[i][num] || col[j][num] || box[box_ind][num]) {
                     return false;
                 }
-            }
-        }
-        return true;
-    }
-private: 
-    bool validate(int top, int down, int left, int right, vector<vector<char>>& board) {
-        vector<bool> record(9, false);
-        for (int i = top; i <= down; i++) {
-            for (int j = left; j <= right; j++) {
-                if (board[i][j] != '.') {
-                    if(record[board[i][j] - '1']) {
-                        return false;
-                    }
-                    record[board[i][j] - '1'] = true;
-                }
+                row[i][num] = col[j][num] = box[box_ind][num] = true;
             }
         }
         return true;
     }
 };
-/*
-vector<bool> record(9, false)
-function validate(top, down, left, right) // range
-    for all element in range
-        check if num[board[i][j]] exists or not
-for each row
-    validate
-for each col
-    validate
-for each square
-    validate
-Time: O(9*9*3)
-Space: O(9)
-*/
