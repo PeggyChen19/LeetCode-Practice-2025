7ad263f25bb58a4f1b5194b05ef2b2a10ab24971
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-21 00:11:45 +0800

    Time: 0 ms (100%), Space: 17.5 MB (88.15%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int size = cost.size();
-        int prev_one = 0, prev_two = 0;
-        for (int i = 2; i < size; i++) {
-            int tmp = prev_one;
-            prev_one = min(prev_one + cost[i-1], prev_two + cost[i-2]);
-            prev_two = tmp;
+        int prev = 0, curr = 0;
+        for (int i = 2; i <= size; i++) {
+            int next = min(curr + cost[i-1], prev + cost[i-2]);
+            prev = curr;
+            curr = next;
         }
-        return min(prev_one + cost[size-1], prev_two + cost[size-2]);
+        return curr;
     }
 };
 /*
-10, 15, 20
-one: 0 10 25
-two: 0 0 15
-
-States: step, total_cost
-dp[s] = min(dp[s-1] + cost[s-1], dp[s-2] + cost[s-2])
-Target: reach size -> come from min(dp[size-1)] + cost[size-1], dp[size-2] + cost[size-2])
+States: step(ind), total_cost
+dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]) // the cost of going to step i
+Target: reach size
 Base case
 dp[0] = 0
 dp[1] = 0
-
 */
