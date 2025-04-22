7920b39d13df23d743bba9959ac9d4e3e8af3832
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-22 14:21:55 +0800

    Time: 8 ms (51.62%), Space: 16.1 MB (42.3%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> word_set(wordDict.begin(), wordDict.end());
-        vector<bool> dp(s.size(), false);
-        for (int i = 0; i < s.size(); i++) {
-            for (int b = -1; b < i; b++) {
-                int len = i - (b+1) + 1;
-                if ( (b == -1 || dp[b]) && word_set.count(s.substr(b+1, len)) ) {
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= s.size(); i++) {
+            for (int b = 0; b < i; b++) {
+                int len = (i-1) - b + 1;
+                if ( dp[b] && word_set.count(s.substr(b, len)) ) {
                     dp[i] = true;
                     break;
                 }
@@ -16,18 +17,19 @@ public:
     }
 };
 /*
-States: ind, valid_ind, break_flag
-Subproblem: if s[0~i] is breakable or not
-Base case: dp[-1] = true
+States: ind, break point, breakable_flag
+Subproblem: if s[0~i-1] is breakable or not
+
+Base case: dp[0] = true // empty string -> breakable
 
 Transition function:
-dp[i] |= dp[vi] && (s[vi+1~i] is in wordDict)
+dp[i] ||= dp[b] && (s[b~i-1] is in wordDict)
 
 Implement:
-for (i = 0; i < size; i++)
-    for (vi = 0; vi = i, vi++)
+for (i = 1; i <= size; i++)
+    for (b = 0; b < i, b++)
         update dp[i]
 
 Memorization:
-vector<bool> dp(s.size(), false)
+vector<bool> dp(s.size()+1, false)
 */
