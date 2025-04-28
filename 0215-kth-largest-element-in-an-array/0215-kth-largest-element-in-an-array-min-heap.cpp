9787c85558eff49d8b16d23a6cf66addbf92b732
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-28 08:48:11 +0800

    Time: 38 ms (35.98%), Space: 61.5 MB (58.37%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array-min-heap.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array-min-heap.cpp
@@ -1,38 +1,20 @@
 class Solution {
-private:
-    int partition(vector<int>& nums, int left, int right) {
-        int pivot = nums[left], l = left + 1, r = right;
-        while (l <= r) {
-            if (nums[l] < pivot && nums[r] > pivot) {
-                swap(nums[l++], nums[r--]);
-            }
-            if (nums[l] >= pivot) {
-                l++;
-            }
-            if (nums[r] <= pivot) {
-                r--;
-            }
-        }
-        swap(nums[left], nums[r]);
-        return r;
-    }
-
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        k--; // because index starts from 0
-        int left = 0, right = nums.size() - 1;
-        while (left <= right) {
-            int pivotInd = partition(nums, left, right);
-            if (pivotInd == k) {
-                return nums[pivotInd];
-            }
-            else if (pivotInd < k) {
-                left = pivotInd + 1;
-            }
-            else {
-                right = pivotInd - 1;
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+        for (int num : nums) {
+            min_heap.push(num);
+            if (min_heap.size() > k) {
+                min_heap.pop();
             }
         }
-        return -1;
+        return min_heap.top();
     }
 };
+/*
+Min heap:
+Time Complexity
+O(nlogk)
+Space Complexity
+O(k)
+*/
