be11d8a1843ea950ab040367829e8cd95131dd43
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-04-29 14:23:48 +0800

    Time: 9 ms (18.24%), Space: 19.8 MB (12.65%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>> graph(n); // src: dst, cost
+        for (auto flight : flights) {
+            graph[flight[0]].push_back({flight[1], flight[2]});
+        }
+        vector<int> dist(n, INT_MAX); // the stop of lowest cost
+        using T = vector<int>;
+        priority_queue<T, vector<T>, greater<T>> min_heap; // cost, stop, node
+        min_heap.push({0, 0, src});
+        while (!min_heap.empty()) {
+            int cost = min_heap.top()[0];
+            int stop = min_heap.top()[1];
+            int node = min_heap.top()[2];
+            min_heap.pop();
+            if (node == dst) return cost;
+            if (stop > k) continue;
+            if (stop >= dist[node]) continue; // top() has relative higher cost and dist -> not possible to be a better path
+            dist[node] = stop; // update stop
+            for (auto [neighbor, weight] : graph[node]) {
+                min_heap.push({cost + weight, stop+1, neighbor});
+            }
+        }
+        return -1;
+    }
+};
+/*
+No negative weight -> Dijsktra
+*/
