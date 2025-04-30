3eb6dacde2af35db6d03d040a13dc05950bc5f55
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-01 01:38:30 +0800

    Time: 6 ms (25.31%), Space: 23.5 MB (41.86%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,28 +1,35 @@
+class Element {
+public:
+    int val, min;
+    Element* next;
+    Element(int val, int min, Element* next): val(val), min(min), next(next) {}
+};
+
 class MinStack {
 private:
-    stack<pair<int, int>> st;
+    Element* top_element;
 public:
     MinStack() {
+        top_element = new Element(0, INT_MAX, nullptr);
     }
     
     void push(int val) {
-        if (st.empty()) {
-            st.push({val, val});
-        } else {
-            st.push({val, min(val, st.top().second)});
-        }         
+        Element* new_element = new Element(val, min(val, top_element->min), top_element);        
+        top_element = new_element;
     }
     
     void pop() {
-        st.pop();
+        Element* to_remove = top_element;
+        top_element = top_element->next;
+        delete to_remove;
     }
     
     int top() {
-        return st.top().first;
+        return top_element->val;
     }
     
     int getMin() {
-        return st.top().second;
+        return top_element->min;
     }
 };
 
@@ -37,4 +44,6 @@ public:
 push, pop, top -> normal stack
 min -> for each element, we can record the min from bottom~element
     -> when we pop, the min record of remaining elements won't affect
+
+we can use build-in stack or create our own via linked list
 */
