12068eda3fceed32a86579da1b7fe33563cc13e3
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-01 17:15:54 +0800

    Time: 4 ms (57.9%), Space: 150.7 MB (45.95%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         bool a = false, b = false, c = false;
-        for (vector<int> t : triplets) {
+        for (vector<int>& t : triplets) {
             if (t[0] <= target[0] && t[1] <= target[1] && t[2] <= target[2]) {
                 if (t[0] == target[0]) a = true;
                 if (t[1] == target[1]) b = true;
