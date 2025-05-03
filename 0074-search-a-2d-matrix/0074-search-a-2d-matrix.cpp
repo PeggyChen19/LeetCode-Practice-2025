b472cd05d1456fcd014b626686568aaf69276240
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-04 01:41:48 +0800

    Time: 0 ms (100%), Space: 13.4 MB (7.54%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,42 +2,21 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
-        vector<int> rowHead(m);
-        for (int i = 0; i < m; i++) {
-            rowHead[i] = matrix[i][0];
-        }
-        int rowInd = binarySearch(rowHead, 0, m-1, target);
-        if (rowInd >= m || matrix[rowInd][0] != target) { // nearest bigger
-            rowInd--;
-        }
-        if (rowInd < 0) { // there is no nearest smaller
-            return false;
-        }
-        int colInd = binarySearch(matrix[rowInd], 0, n-1, target);
-        return (colInd >= n || matrix[rowInd][colInd] != target) ? false : true;
-    }
-private: 
-    int binarySearch(vector<int>& vec, int left, int right, int target) {
+        int left = 0, right = m * n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (vec[mid] < target) { // go right
+            int mid_val = matrix[mid / n][mid % n];
+            if (mid_val < target) { // go right
                 left = mid + 1;
-            } else if (vec[mid] > target) { // go left
+            } else if (mid_val > target) { // go left
                 right = mid - 1;
             } else {
-                return mid;
+                return true;
             }
         }
-        return left;
+        return false;
     }
 };
 /*
-Binary search1: find the possbile row
-Binary search2: find the target in the possible row
-1:
-Goal: find rowHead == target OR rowHead nearest to target but smaller
-if we return left -> the nearest larger one
-so we can use left-1 represent the nearest smaller one
-2:
-ending condition: matrix[i][j] == target
+We can treat it as a 1D vector and then use binary search to achieve O(log(m*n))
 */
