ecfc2271b447180e661bd02e281528809041659d
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-06 18:26:44 +0800

    Time: 3 ms (5.17%), Space: 11.4 MB (65.62%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-	vector<int> plusOne(vector<int>& digits) {
-		int carry = 1;
-		for (int i = digits.size() - 1; i >= 0; i--) {
-			digits[i] += carry;
-			carry = digits[i] / 10;
-			digits[i] %= 10;
-			if (!carry) {
-				break;
-			}
-		}
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            digits[i] += carry;
+            carry = digits[i] / 10;
+            digits[i] %= 10;
+            if (!carry) {
+                break;
+            }
+        }
         if (carry) {
             digits.insert(digits.begin(), 1);
         }
-		return digits;
-	}
+        return digits;
+    }
 };
