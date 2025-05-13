81905a54ce18012c9d355bd3724dae2df89a2cac
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-13 20:45:38 +0800

    Time: 0 ms (100%), Space: 11.2 MB (72.32%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,34 +1,35 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if (left == right) { // don't need reverse
+        if (left == right) { // not need reverse
             return head;
         }
-        ListNode* cur = head;
-        for (int i = 2; i < left; i++) { // cur will stop at left-1
-            cur = cur->next;
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        ListNode* pre = dummy;
+        for (int i = 1; i < left; i++) { // cur will stop at left-1
+            pre = pre->next;
         }
-        ListNode* left_end = cur;
-        cur = cur->next;
-        ListNode* pre = nullptr;
-        for (int i = 0; i < (right-left+1); i++) {
-            ListNode* next = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = next;
+        ListNode* cur = pre->next;
+        for (int i = 0; i < (right - left); i++) {
+            ListNode* rangeHead = cur->next;
+            cur->next = rangeHead->next;
+            rangeHead->next = pre->next;
+            pre->next = rangeHead;
         }
-        left_end->next->next = cur; // cur is right_start
-        left_end->next = pre; // pre is right
-        return head;
+        return dummy->next;
     }
 };
+/*
+直接用原本的 reverse 會變得很複雜 -> Head Insertion
+Head Insertion: reverse a sublist in a linked list
+1. Find Pointers
+    a. pre: the node before the sublist
+    b. cur: the first node in the sublist
+2. Repeat right - left times:
+    a. find the future new head of the sublist: rangeHead = cur->next
+    b. detach rangeHead: cur->next = rangeHead->next: 
+    c. insert rangeHead right after pre:
+        rangeHead->next = pre->next
+        pre->next = rangeHead    
+*/
