7e900137afe5675be97041a293f76d51003ac1be
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-15 18:22:14 +0800

    Time: 5 ms (46.64%), Space: 18.1 MB (76.19%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -3,34 +3,41 @@ public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> result;
         vector<vector<int>> graph(numCourses);
-        vector<int> inDegree(numCourses, 0);
         for (auto& pre : prerequisites) {
             graph[pre[1]].push_back(pre[0]);
-            inDegree[pre[0]]++;
         }
-        queue<int> q;
+        vector<bool> visited(numCourses, false);
+        vector<bool> onPath(numCourses, false);
         for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                q.push(i);
+            if (visited[i]) {
+                continue;
+            }
+            if (hasCycle(i, graph, visited, onPath, result)) {
+                return {};
             }
         }
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            result.push_back(node);
-            for (auto& neighbor : graph[node]) {
-                inDegree[neighbor]--;
-                if (inDegree[neighbor] == 0) {
-                    q.push(neighbor);
-                }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+private:
+    bool hasCycle(int course, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& onPath, vector<int>& result) { 
+        if (onPath[course]) return true; // detect cycle
+        if (visited[course]) return false;
+        visited[course] = true;
+        onPath[course] = true;
+        for (auto& neighbor : graph[course]) {
+            if (hasCycle(neighbor, graph, visited, onPath, result)) {
+                return true;
             }
         }
-        return (result.size() == numCourses) ? result : vector<int>{};
+        onPath[course] = false; // recover
+        result.push_back(course);
+        return false;
     }
 };
 /*
 [0, 1]: 1 must before 0 (1->0)
 prerequisites actually represent a graph
-build this graph based on adjancent list
-we need to find the topological ordering and check if there is any cycle -> BFS
+build this graph based on adjacent list
+we need to find the topological ordering and check if there is any cycle -> BFS / DFS
 */
