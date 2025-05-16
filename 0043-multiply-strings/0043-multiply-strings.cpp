15be2cd5410c0df35d26ffb0bdf6579d8f694f80
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-16 10:21:09 +0800

    Time: 3 ms (60.02%), Space: 10.5 MB (42.73%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int len1 = num1.size(), len2 = num2.size();
+        vector<int> result(len1 + len2, 0);
+        for (int i = len1-1; i >= 0; i--) {
+            for (int j = len2-1; j >= 0; j--) {
+                result[i+j+1] += (num1[i] - '0') * (num2[j] - '0');
+            }
+        }
+        for (int i = len1+len2-1; i > 0; i--) {
+            result[i-1] += (result[i] / 10);
+            result[i] %= 10;
+        }
+        string ans = "";
+        for (int i = 0; i < len1+len2; i++) {
+            if (ans == "" && result[i] == 0) { // ignore leading zero
+                continue;
+            }
+            ans += to_string(result[i]);
+        }
+        return ans == "" ? "0" : ans;
+    }
+};
+/*
+at most len1 + len2 digits
+for nums[i] * nums[j] -> store in i+j+1 (because it's 0 indexed)
+    0 1 2
+    1 2 3
+      4 5
+----------
+    * * *
+  * * *  
+----------
+0 1 2 3 4
+Create a result vector and then handle the carries after calculating all digits
+*/
