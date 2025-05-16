4dd5153ab226af3e9161c8abc63714d51ca89207
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-16 10:22:13 +0800

    Time: 4 ms (34.73%), Space: 10.6 MB (41.27%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        if (num1 == "0" || num2 == "0") {
-            return "0";
-        }
         int len1 = num1.size(), len2 = num2.size();
         vector<int> result(len1 + len2, 0);
         for (int i = len1-1; i >= 0; i--) {
