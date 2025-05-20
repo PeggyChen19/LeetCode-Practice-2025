0e305e9bc7313d49709f8ed35ed03b2fd1f7f539
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-20 17:27:33 +0800

    Time: 103 ms (38.29%), Space: 88.5 MB (27.77%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,32 @@
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+*     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+*     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+* };
+*/
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        return inorder(root, INT_MIN);
+    }
+private:
+    int inorder(TreeNode* node, int curMax) {
+        if (!node) {
+            return 0;
+        }
+        int goodNode = (node->val >= curMax);
+        curMax = max(node->val, curMax);
+        return goodNode + inorder(node->left, curMax) + inorder(node->right, curMax);
+    }
+};
+/*
+Good Node: no greater node on the path
+
+In-order go through all nodes  -> recursion
+Record the max value until now
+*/
