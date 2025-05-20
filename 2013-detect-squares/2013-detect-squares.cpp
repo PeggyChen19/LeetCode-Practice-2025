bc2841a25293ba3c342f87a12a1117ea31f33a8f
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-20 15:52:01 +0800

    Time: 19 ms (95.19%), Space: 102.3 MB (43.15%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.cpp
+++ 2013-detect-squares/2013-detect-squares.cpp
@@ -1,32 +1,26 @@
 class DetectSquares {
 private:
-    unordered_map<string, int> counts; // point, count
-    unordered_map<int, vector<int>> x_point; // x, vector y
+    unordered_map<int, unordered_map<int, int>> points; // (x, (y, count)
 public:
     DetectSquares() {}
     
     void add(vector<int> point) {
         int x = point[0];
         int y = point[1];
-        counts[convertFormat(point)]++;
-        x_point[x].push_back(y);
+        points[x][y]++;
     }
-    
+
     int count(vector<int> point) {
         int result = 0;
         int x = point[0];
         int y = point[1];
-        for (auto& y_candidate : x_point[x]) {
-            int len = y_candidate - y;
+        for (auto& [y2, y2Count] : points[x]) {
+            int len = y2 - y;
             if (len == 0) continue;
-            result += (counts[convertFormat({x + len, y})] * counts[convertFormat({x + len, y_candidate})]);
-            result += (counts[convertFormat({x - len, y})] * counts[convertFormat({x - len, y_candidate})]);
+            result += y2Count * points[x + len][y] * points[x + len][y2];
+            result += y2Count * points[x - len][y] * points[x - len][y2];
         }
         return result;
-    }
-private:
-    string convertFormat(vector<int> point) {
-        return to_string(point[0]) + "#" + to_string(point[1]);
     }
 };
 /**
@@ -37,13 +31,12 @@ Count:
 Given point (x, y)
 find all points with the same x
     find the other two points to build a square
+    consider counts
 
 Which data structure to store points?
 Find (x, y), (x, _) quickly
 Store duplicated points
-
-unordered_map<pair<int, int>, int> // (x, y), count
-unordered_map<int, vector<int>> // x, y
+-> unordered_map<int, unordered_map<int, int>> -> (x, (y, count)
 
 * Your DetectSquares object will be instantiated and called as such:
 * DetectSquares* obj = new DetectSquares();
