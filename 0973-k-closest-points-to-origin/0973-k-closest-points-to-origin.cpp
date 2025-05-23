b63d27bedb36ca85b7d2dc8a642784f8000f829c
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-23 15:17:57 +0800

    Time: 92 ms (34.41%), Space: 78.1 MB (35.36%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        using T = pair<int, vector<int>>;
-        vector<T> distance;
-        vector<vector<int>> results;
-        for (int i = 0; i < points.size(); i++) {
-            int dist = points[i][0] * points[i][0] + points[i][1] * points[i][1];
-            distance.push_back({dist, points[i]});
+        using T = pair<int, vector<int>>; // distance, point vector
+        priority_queue<T> maxHeap;
+        for (auto& point : points) {
+            int dist = point[0] * point[0] + point[1] * point[1];
+            maxHeap.push({dist, point});
+            if (maxHeap.size() > k) {
+                maxHeap.pop();
+            }
         }
-        priority_queue<T, vector<T>, greater<T>> minHeap(distance.begin(), distance.end());
-        for (int i = 0; i < k; i++) { // do it k times
-            results.push_back(minHeap.top().second);
-            minHeap.pop();
+        vector<vector<int>> results;
+        while (!maxHeap.empty()) {
+            results.push_back(maxHeap.top().second);
+            maxHeap.pop();
         }
         return results;
     }
 };
 /*
-For comparing distance, we can use points[0]^2 + points[1]^2
-(Don't need square root because it won't affect the order of their relative magnitudes)
-Top K small values -> priority queue / quick select
-Priority Queue - Min Heap
-Put the whole vector into heap: O(n)
-Retrieve the kth element: O(klogn)
+Priority Queue - Max Heap
+Push the element one by one 
+    when size == k
+        pop
+O(nlogk)
 */
