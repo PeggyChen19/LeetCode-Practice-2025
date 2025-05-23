fd269e57d57bf6eaa465c42bdd1c50cf49ef5ed0
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-23 16:10:22 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,27 +1,61 @@
 class Solution {
+private:
+    int quickSelect(int left, int right, vector<pair<int, vector<int>>>& dist) {
+        int pivot = dist[left].first, l = left + 1, r = right;
+        while (l <= r) {
+            if (dist[l].first > pivot && dist[r].first < pivot) {
+                swap(dist[l], dist[r]);
+                l++;
+                r--;
+            }
+            if (dist[l].first <= pivot) l++;
+            if (dist[r].first >= pivot) r--;
+        }
+        swap(dist[left], dist[r]);
+        return r;
+    }
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        using T = pair<int, vector<int>>; // distance, point vector
-        priority_queue<T> maxHeap;
+        vector<pair<int, vector<int>>> dist; // distance, point vector
         for (auto& point : points) {
-            int dist = point[0] * point[0] + point[1] * point[1];
-            maxHeap.push({dist, point});
-            if (maxHeap.size() > k) {
-                maxHeap.pop();
+            int distance = point[0] * point[0] + point[1] * point[1];
+            dist.push_back({distance, point});
+        }
+        int left = 0, right = points.size() - 1;
+        while (true) {
+            int pivotInd = quickSelect(left, right, dist);
+            if (pivotInd == k) {
+                break;
+            } else if (pivotInd > k) { // go left
+                right = pivotInd - 1;
+            } else { // go right
+                left = pivotInd + 1;
             }
         }
         vector<vector<int>> results;
-        while (!maxHeap.empty()) {
-            results.push_back(maxHeap.top().second);
-            maxHeap.pop();
+        for (int i = 0; i < k; i++) {
+            results.push_back(dist[i].second);
         }
         return results;
     }
 };
 /*
-Priority Queue - Max Heap
-Push the element one by one 
-    when size == k
-        pop
-O(nlogk)
+Quick Select
+int quickSelect(left, right) // small to big
+    pivot = dist[left], l = left+1, r = right
+    while (l <= r)
+        if (dist[l] > pivot && dist[r] < pivot)
+            swap(dist[l], dist[r])
+            l++
+            r--
+        if (dist[l] <= pivot)
+            l++
+        if (dist[r] >= pivot)
+            r--
+    swap(dist[left], dist[r]) // [r l] at this time (r <= pivot -> swap to left side)
+    return r;
+
+main func
+    pivotInd = quickSelect(start, end)
+    adjust range based on pivotInd
 */
