d3f0bc3f079b35c415f88b239f4b2a8adde1fe2d
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-25 00:54:11 +0800

    Time: 0 ms (100%), Space: 20.4 MB (84.71%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -17,4 +17,12 @@ public:
 Target: min jumps to reach nums[n-1]
 DP / Greedy -> Greedy can do it, don't need DP
 Concept: keep finding the point which can go farthest
+
+curFarthest: the end of the current jump range. 
+             When we reach this point, we must jump, and update it to nextFarthest.
+
+nextFarthest: the farthest point we can reach within the current range.
+              It keeps updating as we explore positions inside the current jump range.
+
+We scan from left to right, always tracking the farthest point we can reach (nextFarthest). When we reach the end of the current range (curFarthest), we must make a jump, and update curFarthest to nextFarthest.
 */
