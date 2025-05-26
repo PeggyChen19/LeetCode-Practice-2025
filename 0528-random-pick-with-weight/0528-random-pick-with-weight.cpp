fb7eb812783dcdb6d36652d0a564161c85bfc700
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-26 14:57:38 +0800

    Time: 9 ms (90.23%), Space: 47.1 MB (47.12%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.cpp
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-	int totalSum;
-	vector<int> prefixSums; // 1 ~ totalSum
+    int totalSum;
+    vector<int> prefixSums; // 1 ~ totalSum
 
-	Solution(vector<int>& w) {
+    Solution(vector<int>& w) {
         srand(time(NULL));
-		totalSum = 0;
-		for (int i = 0; i < w.size(); i++) {
-			totalSum += w[i];
-			prefixSums.push_back(totalSum);
-		}
-	}
-	
-	int pickIndex() {
-		int min = 1, max = totalSum; // the same range as prefixSums
-		int target = rand() % (max - min + 1) + min;
-		return binarySearch(target);
-	}
+        totalSum = 0;
+        for (int i = 0; i < w.size(); i++) {
+            totalSum += w[i];
+            prefixSums.push_back(totalSum);
+        }
+    }
+
+    int pickIndex() {
+        int min = 1, max = totalSum; // the same range as prefixSums
+        int target = rand() % (max - min + 1) + min;
+        return binarySearch(target);
+    }
 
 private:
-	int binarySearch(int& target) { // find the index <= target
-		int left = 0, right = prefixSums.size() - 1;
+    int binarySearch(int& target) { // find the index <= target
+        int left = 0, right = prefixSums.size() - 1;
         int result = -1;
-		while (left <= right) {
-			int mid = left + (right - left) / 2;
-			if (target <= prefixSums[mid]) { // go left
-                result = mid; // probably valid -> update result
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target <= prefixSums[mid]) { // go left
+                result = mid;                // probably valid -> update result
                 right = mid - 1;
-			} else { // go right
+            } else { // go right
                 left = mid + 1;
             }
-		}
-		return result;
-	}
+        }
+        return result;
+    }
 };
 
 /*
