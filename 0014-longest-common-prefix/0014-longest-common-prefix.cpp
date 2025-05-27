8723b2fa1e04c54c8cbf95e6682ce59d745d731f
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-05-27 11:34:55 +0800

    Time: 0 ms (100%), Space: 11.9 MB (67.49%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs[0].size() == 0) {
+            return "";
+        }
+        int commonInd = strs[0].size() - 1;
+        for (int i = 1; i < strs.size(); i++) {
+            for (int j = 0; j <= commonInd; j++) {
+                if (strs[0][j] != strs[i][j]) {
+                    commonInd = j - 1;
+                }
+            }
+        }
+        return strs[0].substr(0, commonInd + 1);
+    }
+};
+/*
+Use a pointer to record the index of longest common prefix
+-> Point at the end of strs[0]
+Go through remaining strs, update the pointer
+*/
