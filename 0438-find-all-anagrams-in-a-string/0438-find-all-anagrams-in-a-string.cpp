a4004738d273ecce234026b4e186e62b4f0ad0af
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-04 15:40:57 +0800

    Time: 6 ms (59.16%), Space: 11.4 MB (84.99%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> results;
+        if (p.size() > s.size()) return results;
+        int unmatched = p.size();
+        unordered_map<char, int> counts;
+        for (char& ch : p) {
+            counts[ch]++;
+        }
+        int start = 0;
+        for (int end = 0; end < s.size(); end++) {
+            if (counts[s[end]] > 0) {
+                unmatched--;
+            }
+            counts[s[end]]--;
+            if (end - start + 1 == p.size()) {
+                if (unmatched == 0) {
+                    results.push_back(start);
+                }
+                if (counts[s[start]] >= 0) {
+                    unmatched++;
+                }
+                counts[s[start]]++;
+                start++;
+            }
+        }
+        return results;
+    }
+};
+/*
+Find the p's anagrams in s (the same components with any order)
+s = "cbaebabacd", p = "abc" -> [0,6]
+Use unordered_map & p.size() to represent p
+Sliding window go through s and find anagrams
+*/
