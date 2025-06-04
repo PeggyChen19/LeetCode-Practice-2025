b4d5eee2d33aa5b9a5c4e1a537f3eb4931bfb40d
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-04 10:45:39 +0800

    Time: 2 ms (82.77%), Space: 17.1 MB (18.69%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,8 +1,9 @@
-class Solution {
-private:
-    static bool customCompare(const string& a, const string& b) {
+struct CustomCompare {
+    bool operator()(const string& a, const string& b) const {
         return (a + b) > (b + a);
     }
+};
+class Solution {
 public:
     string largestNumber(vector<int>& nums) {
         string result = "";
@@ -10,7 +11,7 @@ public:
         for (auto& num : nums) {
             strNums.push_back(to_string(num));
         }
-        sort(strNums.begin(), strNums.end(), customCompare);
+        sort(strNums.begin(), strNums.end(), CustomCompare());
         if (strNums.empty() || strNums[0] == "0") {
             return "0";
         }
@@ -20,10 +21,3 @@ public:
         return result;
     }
 };
-/*
-We need to sort nums in some special rules
-99, 900 -> 99 go first
-987, 9876 -> 987 go first (9879876 > 9876987)
-123, 1234 -> 1234 go first (1234123 > 1231234)
-Based on string compare, but not simply compare s1 & s2 -> s1+s2 & s2+s1
-*/
