5c265757f064049f008099d6e0a8a3c316a1d835
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-04 18:24:58 +0800

    Time: 0 ms (100%), Space: 35.9 MB (66.8%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,53 +1,26 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int> smaller, bigger;
-        int ind = binarySearch(arr, x);
-        int left = ind;
-        int right = ind + 1;
-        while (k > 0) {
-            if (left >= 0 && right < arr.size()) { // both valid -> compare
-                int lDis = x - arr[left];
-                int rDis = arr[right] - x;
-                if (lDis <= rDis) {
-                    smaller.push_back(arr[left]);
-                    left--;
-                } else {
-                    bigger.push_back(arr[right]);
-                    right++;
-                }
-            } else if (left >= 0) {
-                smaller.push_back(arr[left]);
-                left--;
-            } else {
-                bigger.push_back(arr[right]);
-                right++;
-            }
-            k--;
-        }
-        reverse(smaller.begin(), smaller.end());
-        smaller.insert(smaller.end(), bigger.begin(), bigger.end());
-        return smaller;
-    }
-private:
-    int binarySearch(vector<int>& arr, int target) {
-        int result = -1, left = 0, right = arr.size() - 1;
+        int left = 0, right = arr.size() - k, ans = -1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] == target) {
-                return mid;
-            } else if (arr[mid] < target) { // go right
-                result = mid;
+            if (mid + k < arr.size() && x - arr[mid] > arr[mid + k] - x) { // go right
                 left = mid + 1;
-            } else { // go left
+            } else { // go left (this condition includes possible ans)
+                ans = mid;
                 right = mid - 1;
             }
         }
-        return result; // the nearest smaller index
+        return vector<int>(arr.begin() + ans, arr.begin() + ans + k);
     }
 };
 /*
 1. find index of x (or nearest point) from the sorted array -> binary search
 2. check the nearest nums in two directions (go to front / go to back)
 3. store in two vector and reverse it and combine
+
+Improvement: We can directly find the start index of result use binary search
+Possible start index range: [0, arr.size - k]
+abs(x - arr[mid]) > abs(x - arr[mid+k]): go right
+else: go left ()
 */
