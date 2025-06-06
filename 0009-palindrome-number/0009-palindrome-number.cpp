53e643d0ab1fc1c9c0e7618087511ac4efdb4aa6
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-06 11:59:05 +0800

    Time: 0 ms (100%), Space: 8.5 MB (92.26%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) return false;
-        int original = x;
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
         int reversed = 0;
-        while (x > 0) {
-            int lastDigit = x % 10;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
             x /= 10;
-            if (reversed > (INT_MAX - lastDigit) / 10) return false; // overflow
-            reversed = reversed * 10 + lastDigit;
         }
-        return original == reversed;
+        return (x == reversed) || (x == reversed / 10);
     }
 };
 /*
 Palindrome -> read the same from left and from right
 Don't convert int to string -> Space O(1)
 Reverse the int and check if it's the same as original one
+Improvement:
+We only need reverse half of the int
 */
