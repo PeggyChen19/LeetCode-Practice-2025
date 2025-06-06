526a03b159075e8eccce0c761b3881e9798bce53
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-06 17:12:58 +0800

    Time: 0 ms (100%), Space: 15.6 MB (92.22%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        bool descending = true;
-        for (int i = nums.size() - 2; i >= 0; i--) {
+        if (nums.empty()) return;
+        int i;
+        for (i = nums.size() - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
-                reverse(nums.begin() + i + 1, nums.end());
-                for (int j = i + 1; j < nums.size(); j++) {
+                for (int j = nums.size() - 1; j >= i + 1; j--) {
                     if (nums[j] > nums[i]) {
                         swap(nums[j], nums[i]);
                         break;
                     }
                 }
-                descending = false;
                 break;
             }
         }
-        if (descending) {
-            reverse(nums.begin(), nums.end());
-        }
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
 /*
@@ -35,8 +32,6 @@ The right part which is descending cannot be bigger via swapping
 -> need to find the pivot and make the pivot position bigger
 
 1. Start from right, find the first nums[i] < nums[i + 1] (pivot)
-2. reverse the right part [i + 1, end] (descending to ascending)
-3. From [i + 1, end], find the first nums[j] > nums[i], swap(nums[i], nums[j])
-
-Specail case: fully descending order: reverse it (become ascending order)
+2. Start from right, find the first nums[j] > nums[i], swap(nums[i], nums[j])
+3. reverse the right part [i + 1, end] (descending to ascending)
 */
