a318a85326e6e302c6098c4a6714e0a763d7715b
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-06 15:29:23 +0800

    Time: 11 ms (64.74%), Space: 14 MB (28.01%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -4,19 +4,18 @@ public:
         int result = nums[0] + nums[1] + nums[2]; // len(nums) >= 3
         sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size(); i++) {
-            int remain = target - nums[i];
             int left = i + 1, right = nums.size() - 1;
             while (left < right) {
-                int twoSum = nums[left] + nums[right];
-                if (twoSum == remain) {
+                int threeSum = nums[i] + nums[left] + nums[right];
+                if (threeSum == target) {
                     return target;
-                } else if (twoSum > remain) { // make two sum smaller
+                } else if (threeSum > target) { // make two sum smaller
                     right--;
                 } else { // make two sum bigger
                     left++;
                 }
-                if (abs(remain - twoSum) < abs(target - result)) { // find better result
-                    result = nums[i] + twoSum;
+                if (abs(threeSum - target) < abs(result - target)) { // find better result
+                    result = threeSum;
                 }
             }
         }
