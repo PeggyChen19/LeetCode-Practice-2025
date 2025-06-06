50909e867de4fa15b52ea36359a5ef1301d41c36
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-06 12:02:10 +0800

    Time: 0 ms (100%), Space: 8.6 MB (43.17%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        if (x == 0) return true;
+        if (x < 0 || x % 10 == 0) return false;
         int reversed = 0;
         while (x > reversed) {
             reversed = reversed * 10 + x % 10;
