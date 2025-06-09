aecda5c027e5cb2cba50548b02e3b808cf32211f
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-09 15:37:35 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        int result = 0;
+        if (!root) return result;
+        queue<pair<TreeNode*, long>> q;
+        q.push({root, 1});
+        while (!q.empty()) {
+            int size = q.size(), left, right;
+            for (int i = 0; i < size; i++) {
+                auto [node, ind] = q.front();
+                q.pop();
+                if (i == 0) left = ind;
+                if (i == size - 1) right = ind;
+                if (node->left) q.push({node->left, ind * 2 - 1});
+                if (node->right) q.push({node->right, ind * 2});
+            }
+            result = max(result, right - left + 1);
+        }
+        return result;
+    }
+};
+/*
+Max width: leftmost ~ rightmost, including null nodes
+Define nodeInd = parentInd * 2 - (is left child)
+BFS: find the left most & right most of each layer
+queue<TreeNode*, int> // node, index
+        1
+       / \
+      1   2
+     / \ / \
+    1  2 3  4
+*/
