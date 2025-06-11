87cc23dafbf934a9d7ab4871f3147be403cc26b1
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-12 01:46:14 +0800

    Time: 29 ms (31.49%), Space: 88.4 MB (45.92%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int maxLen = 0;
+        unordered_map<int, int> firstPrefixSum; // prefixSum, firstInd
+        int curPrefixSum = (nums[0] == 1) ? 1: -1;
+        firstPrefixSum[curPrefixSum] = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            curPrefixSum += (nums[i] == 1) ? 1: -1;
+            if (curPrefixSum == 0) {
+                maxLen = max(maxLen, i + 1);
+            } else if (firstPrefixSum.count(curPrefixSum)) {
+                maxLen = max(maxLen, i - firstPrefixSum[curPrefixSum]);
+            }
+            if (!firstPrefixSum.count(curPrefixSum)) {
+                firstPrefixSum[curPrefixSum] = i;
+            }
+        }
+        return maxLen;
+    }
+};
+/*
+-1 -1  1  1  -1 
+-1 -2 -1  0  -1
+Subarray -> Sliding Window / DP / Prefix Sum
+Not Sliding Window -> can't decide when to move left
+Not DP -> ??
+Target: find the max subarray where sum == 0 (replace 0 to -1)
+interval sum = curPrefixSum - prefixSum = 0
+
+int maxLen = 0
+unordered_map<int, int> firstPrefixSum // prefixSum, firstInd
+go through nums
+    record curPrefixSum
+    find -curprefixSum from firstPrefixSum
+    update firstPrefixSum
+*/
