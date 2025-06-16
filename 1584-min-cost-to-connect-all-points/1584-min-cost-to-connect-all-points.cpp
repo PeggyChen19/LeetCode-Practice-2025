29eb7dcfd4a2513eb4d8765d51d4c1d990000944
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-16 16:51:29 +0800

    Time: 23 ms (94.14%), Space: 18.3 MB (80.12%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int total = 0, n = points.size();
+        int total = 0, n = points.size(), edge = 0;
         vector<int> visited(n, false);
         vector<int> minDist(n, INT_MAX); // min dist from a node to any nodes in MST
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap; // weight, node
         minHeap.push({0, 0});
-        while (!minHeap.empty()) {
+        while (edge < n) {
             auto [w, node] = minHeap.top();
             minHeap.pop();
             if (visited[node]) continue;
             visited[node] = true;
             total += w;
+            edge++;
             for (int i = 0; i < n; i++) {
                 if (visited[i]) continue;
                 int dist = abs(points[node][0] - points[i][0]) + abs(points[node][1] - points[i][1]);
@@ -29,17 +30,6 @@ Minimum Spanning Tree
 Every two points can build an edge
 n points, n*(n-1)/2 edges
 Dense Graph -> Prim's algorithm
-
-Build visited vector
-Build min heap
-Build minDist vector // min dist from a node to any nodes in MST
-push the first point into minHeap
-while !minHeap.empty()
-    retrieve minHeap.top
-    check if top is visited or not
-    update MST
-    for all neighbors
-        push into minHeap if not visited
 
 Note: don't need to record the weights in advance, calculate it when needed
 
