608979b55721da9a00d868481aa6eb999daa8d98
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-16 17:49:06 +0800

    Time: 651 ms (24.11%), Space: 180.5 MB (18.33%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,40 +1,58 @@
 class Solution {
+private:
+    int find(int a, vector<int>& parent) {
+        if (a != parent[a]) {
+            parent[a] = find(parent[a], parent);
+        }
+        return parent[a];
+    }
+    bool unite(int a, int b, vector<int>& parent, vector<int>& rank) {
+        int rootA = find(a, parent), rootB = find(b, parent);
+        if (rootA == rootB) return false; // already in the same set
+        if (rank[rootA] > rank[rootB]) {
+            parent[rootB] = rootA;
+        } else if (rank[rootA] < rank[rootB]) {
+            parent[rootA] = rootB;
+        } else {
+            parent[rootA] = rootB;
+            rank[rootB]++;
+        }
+        return true;
+    }
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int total = 0, n = points.size(), count = 0;
-        vector<int> visited(n, false);
-        vector<int> minDist(n, INT_MAX); // min dist from a node to any nodes in MST
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap; // weight, node
-        minDist[0] = 0;
-        minHeap.push({0, 0});
-        while (count < n) {
-            auto [w, node] = minHeap.top();
-            minHeap.pop();
-            if (visited[node]) continue;
-            visited[node] = true;
-            total += w;
-            count++;
-            for (int i = 0; i < n; i++) {
-                if (visited[i]) continue;
-                int dist = abs(points[node][0] - points[i][0]) + abs(points[node][1] - points[i][1]);
-                if (dist < minDist[i]) {
-                    minDist[i] = dist;
-                    minHeap.push({dist, i});
-                }
+        int v = points.size();
+        vector<int> parent(v);
+        vector<int> rank(v, 1);
+        for (int i = 0; i < v; i++) {
+            parent[i] = i;
+        }
+        vector<vector<int>> edges; // weight, node(smaller ind), node(bigger ind), 
+        for (int i = 0; i < v; i++) {
+            for (int j = i + 1; j < v; j++) {
+                int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+                edges.push_back({dist, i, j});
             }
+        }
+        sort(edges.begin(), edges.end());
+        int count = 0, total = 0;
+        for (auto& edge : edges) {
+            int w = edge[0], n1 = edge[1], n2 = edge[2];
+            if (unite(n1, n2, parent, rank)) {
+                count++;
+                total += w;
+            }
+            if (count == v - 1) break;
         }
         return total;
     }
 };
 /*
-Minimum Spanning Tree
-Every two points can build an edge
-n points, n*(n-1)/2 edges
-Dense Graph -> Prim's algorithm
-
-Note: don't need to record the weights in advance, calculate it when needed
-
-Time Complexity: O(n^2 logn)
-但是實際上 push 跟 pop 次數會分別大約做 n 次，檢查鄰居的成本是 n^2 -> 平均會接近 O(n^2)
-Space Complexity: O(n)
+Kruskal's algo for MST
+Set up union find
+Sort the edges
+For each edge (from small to big)
+    united two sets
+    update MST
+    break when number of edges in MST == n-1
 */
