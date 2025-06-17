f52937d807e76851466291392053282c7d592fa6
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-17 10:38:43 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -5,15 +5,12 @@ public:
         ListNode* oddCur = head;
         ListNode* evenHead = head->next;
         ListNode* evenCur = evenHead;
-        ListNode* cur = evenHead->next;
-        while (cur) {
-            oddCur->next = cur;
-            oddCur = cur;
-            cur = cur->next;
-            evenCur->next = cur;
-            evenCur = cur;
-            if (!cur) break;
-            cur = cur->next;
+        while (evenCur) {
+            oddCur->next = evenCur->next;
+            oddCur = evenCur->next;
+            if (!oddCur) break;
+            evenCur->next = oddCur->next;
+            evenCur = oddCur->next;
         }
         oddCur->next = evenHead;
         return head;
@@ -21,18 +18,17 @@ public:
 };
 /*
 odd -> even
-1 2 3 4 5
-Create some pointers: oddCur, evenCur, cur
+1 2 3 4
+Create some pointers: oddCur, evenCur, evenHead
 oddCur = head
-evenCur = head->next (nullptr check)
-cur = head->next->next
-while cur
-    oddCur->next = cur
-    oddCur = cur
-    cur = cur->next (nullptr check)
-    evenCur->next = cur
-    evenCur = cur
-    cur = cur->next
+evenHead = evenCur = head->next
+while evenCur
+    oddCur->next = evenCur->next
+    oddCur = evenCur->next
+    if (!oddCur) break
+    evenCur->next = oddCur->next
+    evenCur = oddCur->next
+oddCur->next = evenHead
 
 Time: O(n)
 Space: O(1)
