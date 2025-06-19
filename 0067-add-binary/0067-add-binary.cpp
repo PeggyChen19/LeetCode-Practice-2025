2de46a1454f03c9c6350de3562bc82e9d0a5d038
Chun Pei, Chen (Peggy) <86610644+moirachen1019@users.noreply.github.com>
2025-06-19 21:35:22 +0800

    Time: 0 ms (100%), Space: 8.9 MB (62.64%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        if (a.size() < b.size()) swap(a, b); // let a.size >= b
+        string result = "";
         int indA = a.size() - 1;
         int indB = b.size() - 1;
         int carry = 0;
-        while (indB >= 0 || carry) {
-            int valB = (indB >= 0 && b[indB] - '0') ? 1 : 0;
-            if (indA < 0) {
-                a = "1" + a;
-                break;
-            }
-            int sum = (a[indA] - '0') + valB + carry;
-            if (sum >= 2) carry = 1;
-            else carry = 0;
-            a[indA] = (sum % 2) ? '1' : '0';
+        while (indA >= 0 || indB >= 0 || carry) {
+            int valA = (indA >= 0 && a[indA] == '1') ? 1 : 0;
+            int valB = (indB >= 0 && b[indB] == '1') ? 1 : 0;
+            int sum = valA + valB + carry;
+            carry = (sum >= 2) ? 1 : 0;
+            result += (sum % 2) ? '1' : '0';
             indA--;
             indB--;
         }
-        return a;
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
 /*
