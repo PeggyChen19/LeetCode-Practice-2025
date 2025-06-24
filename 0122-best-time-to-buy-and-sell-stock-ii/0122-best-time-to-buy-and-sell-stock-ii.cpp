475022c40192c420901d1df44932489953ac7b6c
Chun Pei, Chen (Peggy) <86610644+PeggyChen19@users.noreply.github.com>
2025-06-24 18:44:30 +0800

    Time: 0 ms (100%), Space: 17.2 MB (67.67%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,26 +2,16 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
-        int minPrice = INT_MAX;
-        for (int i = 0; i < prices.size() - 1; i++) {
-            minPrice = min(minPrice, prices[i]);
-            if (prices[i] > prices[i + 1]) {
-                profit += (prices[i] - minPrice);
-                minPrice = INT_MAX;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i - 1] < prices[i]) {
+                profit += (prices[i] - prices[i - 1]);
             }
-        }
-        if(minPrice != INT_MAX) {
-            profit += (prices.back() - minPrice);
         }
         return profit;
     }
 };
 /*
-On each day, you may decide to buy and/or sell the stock. 
-You can only hold at most one share of the stock at any time. 
-Find and return the maximum profit you can achieve.
-
-[7   1   5   7   3   6   9]
 1. keep recording the min point
 2. when encountering a peak -> sell it (profit = peak - min)
+Improvement: don't need to find the exactly min point & peak -> just sum up all increasing intervals
 */
