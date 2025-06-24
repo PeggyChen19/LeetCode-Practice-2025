499082b7bb539cede8b774874940910ee56af1b4
Chun Pei, Chen (Peggy) <86610644+PeggyChen19@users.noreply.github.com>
2025-06-24 14:49:47 +0800

    Time: 12 ms (17.97%), Space: 12.1 MB (67.96%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> roman;
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
+        int result = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (i + 1 < s.size() && roman[s[i]] < roman[s[i + 1]]) {
+                result -= roman[s[i]];
+            } else {
+                result += roman[s[i]];
+            }
+        }
+        return result;
+    }
+};
+/*
+Sum up all chars
+When encountering I, X, C, check the next char to decide to add or reduce
+*/
