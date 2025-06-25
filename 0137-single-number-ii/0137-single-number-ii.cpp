2c3d6ec38bc67b0ef37d9abeaeb92b94da0cd72c
Chun Pei, Chen (Peggy) <86610644+PeggyChen19@users.noreply.github.com>
2025-06-25 17:43:15 +0800

    Time: 0 ms (100%), Space: 13.4 MB (64.77%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -13,8 +13,19 @@ public:
 1 num: appears once, other nums: appears 3 times 
 Time: O(n), Space: (1)
 
-Bit manipulation (XOR)? not work, we can't tell the difference between 1 or 3 times
-Sum up nums and mod 3? not work, for nums which can divide by 3 itself, we can't filter out it
--> track how many times each bit has appeared, and then mod 3 
-How? use two integers: once and twice to record bit states
+初始想法：Bit manipulation (XOR)? not work, we can't tell the difference between 1 or 3 times
+初始想法：Sum up nums and mod 3? not work, for nums which can divide by 3 itself, we can't filter out it
+
+修正：Sum up nums in bit level and then mod 3 
+How? 每個 int 都是由 32 bits 組成，所以可以用來記錄每個 bit 的狀態 -> use two int: once & twice
+times once twice
+0     0    0
+1     1    0
+2     0    1
+once = (once ^ num) & ~twice
+^：只考慮剛好一個 1 的情況，兩個 1 在 twice 處理
+& ~twice：once 和 twice 不會同時為 1
+twice = (twice ^ num) & ~once
+^：兩個 1（2+1=3）或沒有 1 都是歸零
+& ~once：once 和 twice 不會同時為 1
 */
