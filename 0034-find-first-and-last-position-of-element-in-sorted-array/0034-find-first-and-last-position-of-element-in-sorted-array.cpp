ea6ba20e0723d10c2ab43e0ed3d49b5ee7f249c0
Chun Pei, Chen (Peggy) <86610644+PeggyChen19@users.noreply.github.com>
2025-06-25 10:53:18 +0800

    Time: 0 ms (100%), Space: 17.6 MB (17.36%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,36 +3,31 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int size = nums.size() - 1;
         int first = binarySearch(nums, 0, size, target, true); // the first element >= target
-        if (first == -1 || nums[first] != target) { // cannot find target
+        if (first > size || nums[first] != target) { // cannot find target
             return {-1, -1};
         }
         int last = binarySearch(nums, first, size, target, false); // the first element > target
-        if (last == -1) { // no element > target
-            last = size;
-        } else {
-            last--;
-        }
+        last--;
         return {first, last};
     }
 private:
     int binarySearch(vector<int>& nums, int left, int right, int target, bool equal) {
-        int mid, result = -1;
+        int mid;
         while (left <= right) {
             mid = left + (right - left) / 2;
             if ((equal && nums[mid] >= target) || (!equal && nums[mid] > target)) { // go left
-                result = mid;
                 right = mid - 1;
             } else { // go right
                 left = mid + 1;
             }
         }
-        return result;
+        return left;
     }
 };
 /*
+1 2 3 3 4 5, 3
 Find the index range of target -> Binary search O(logn)
 Do the binary search twice
 1. Find the first element >= target
 2. Find the first element > target
-1 2 3 3 4 5
 */
