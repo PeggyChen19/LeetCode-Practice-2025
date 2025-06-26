277651b2f32316a07275bd01b05b861a0624af12
Chun Pei, Chen (Peggy) <86610644+PeggyChen19@users.noreply.github.com>
2025-06-26 11:12:03 +0800

    Time: 0 ms (100%), Space: 12.5 MB (66.67%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,15 +5,13 @@ public:
         int left = 0, right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == n - 1 || nums[mid] > nums[mid + 1])) {
-                return mid; // peak
-            } else if (mid == 0 || nums[mid - 1] < nums[mid]) { // go right
-                left = mid + 1;
-            } else { // go left
+            if (mid == n - 1 || nums[mid] > nums[mid + 1]) { // go left
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return -1;
+        return left; // the first element which nums[mid] > nums[mid+1] -> peak
     }
 };
 /*
@@ -21,7 +19,6 @@ time: O(logn)
 binary search
 for a peak: (up: up, d: down)
 u u * d d d u u u * d d d
-if (nums[i] > nums[i-1] && nums[i] > nums[i + 1]) return peak
-else if (nums[i - 1] < nums[i]) up -> go right
-else if (nums[i - 1] > nums[i]) down -> go left
+if (nums[i] > nums[i+1]) down -> go left
+else up -> go right
 */
