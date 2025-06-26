f6a04ce429a6f0448db70ee5636351a379f8af69
Chun Pei, Chen (Peggy) <86610644+PeggyChen19@users.noreply.github.com>
2025-06-26 10:24:33 +0800

    Time: 3 ms (31.36%), Space: 15.4 MB (90.76%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<int> dp(n + 1, INT_MAX);
-        dp[1] = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[j + 1] = min(dp[j], dp[j + 1]) + grid[i][j];
+        vector<int> dp(n, 0);
+        dp[0] = grid[0][0];
+        for (int j = 1; j < n; j++) {
+            dp[j] = dp[j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = min(dp[j - 1], dp[j]) + grid[i][j];
             }
         }
-        return dp[n];
+        return dp[n - 1];
     }
 };
 /*
@@ -17,6 +21,8 @@ Shortest path -> DP
 for each cell, calculate the minPath sum
 Transition Function:
 sum[i][j] = min(sum[i][j-1], sum[i-1][j]) + grid[i][j]
+Base Case:
+sum[0][j] = sum[0][j-1] + grid[0][j]
 Memorization:
 1D vector
 */
