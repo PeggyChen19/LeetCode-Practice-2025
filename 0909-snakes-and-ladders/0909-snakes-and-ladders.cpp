2d21b68626cc12a957e041c16a1a1e70c52b5a83
Chun Pei, Chen (Peggy) <86610644+PeggyChen19@users.noreply.github.com>
2025-06-29 15:55:06 +0800

    Time: 3 ms (50.12%), Space: 16.1 MB (94.27%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
-        int n = board.size();
-        vector<bool> visited(n * n, false);
+        int steps = 0, n = board.size();
+        int dst = n * n - 1;
+        vector<bool> visited(dst, false);
         queue<int> q;
         q.push(0);
         visited[0] = true;
-        int steps = 0;
-
         while (!q.empty()) {
             int size = q.size();
             for (int i = 0; i < size; i++) {
                 int cell = q.front();
                 q.pop();
-                if (cell == n * n - 1) return steps;
-
+                if (cell == dst) return steps;
                 for (int dice = 1; dice <= 6; dice++) {
                     int next = cell + dice;
-                    if (next >= n * n) continue;
-
-                    int real = getBoardValue(board, next, n);
-                    if (!visited[real]) {
-                        visited[real] = true;
-                        q.push(real);
-                    }
+                    if (next > dst || visited[next]) continue;
+                    visited[next] = true;
+                    int val = findBoardVal(board, next, n);
+                    if (val == -1) q.push(next); // no ladder or snake
+                    else q.push(val); // ladder -> go to dst
                 }
             }
             steps++;
         }
-
         return -1;
     }
-
 private:
-    int getBoardValue(const vector<vector<int>>& board, int idx, int n) {
-        int r = n - 1 - (idx / n);
-        int c = (idx / n) % 2 == 0 ? idx % n : n - 1 - (idx % n);
-        return board[r][c] == -1 ? idx : board[r][c] - 1;
+    int findBoardVal(const vector<vector<int>>& board, const int& index, const int& n) {
+        int divide = index / n;
+        int mod = index % n;
+        int x = (n - 1) - divide;
+        int y = (divide % 2) ? (n - 1) - mod : mod;
+        return board[x][y] == -1 ? -1 : board[x][y] - 1;
     }
 };
+/*
+Limitation:
+Cannot take the ladder more than once in the same step
+
+Greedy (use BFS)
+Use a queue to record all reachable cells of each round
+For each cell, expand the possible next cells (i+1 ~ i+6 + ladder's dst)
+*/
