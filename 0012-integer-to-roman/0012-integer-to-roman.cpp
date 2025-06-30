ace1b5f4df3e7cf1fe69b154224043f46e037125
Chun Pei, Chen (Peggy) <86610644+PeggyChen19@users.noreply.github.com>
2025-06-30 11:01:50 +0800

    Time: 0 ms (100%), Space: 12.6 MB (42.92%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        if (num < 1 || num > 3999) return "";
         string result = "";
-        unordered_map<int, string> roman;
-        roman[1] = "I"; roman[5] = "V";
-        roman[10] = "X"; roman[50] = "L";
-        roman[100] = "C"; roman[500] = "D";
-        roman[1000] = "M";
-        vector<int> ones = {1000, 100, 10, 1};
-        vector<int> fives = {-1, 500, 50, 5};
-        for (int i = 0; i < ones.size(); i++) {
-            int divide = num / ones[i];
-            num %= ones[i];
-            if (divide == 4) {
-                result += roman[ones[i]] + roman[fives[i]];
-            } else if (divide == 9) {
-                result += roman[ones[i]] + roman[ones[i - 1]];
-            } else {
-                if (divide >= 5) {
-                    result += roman[fives[i]];
-                    divide -= 5;
-                }
-                for (int j = 0; j < divide; j++) {
-                    result += roman[ones[i]];
-                }
+        vector<pair<int, string>> roman = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+        };
+        for (auto& [val, symbol] : roman) {
+            while (num >= val) {
+                result += symbol;
+                num -= val;
             }
         }
         return result;
@@ -32,11 +18,6 @@ public:
 };
 /*
 1 <= num <= 3999
-ones: 1000, 100, 10, 1
-fives: 500, 50, 5
-for ones
-    if num / one > 0
-        if num / one == 4: use (one - five)
-        if num / one == 5: use five
-        else: use multiple ones
+set a vector to record val: symbol
+convert val to symbol from big to small
 */
