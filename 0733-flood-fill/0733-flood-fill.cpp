0508288a06eb7911d3f3b2b3b430cdcbf57f83be
Chun Pei, Chen (Peggy) <86610644+PeggyChen19@users.noreply.github.com>
2025-06-30 17:24:35 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int m = image.size(), n = image[0].size();
         int original = image[sr][sc];
         if (original == color) return image; // no need to change
         queue<pair<int, int>> q;
@@ -9,12 +10,11 @@ public:
         while (!q.empty()) {
             auto [x, y] = q.front();
             q.pop();
-            image[x][y] = color;
             for (auto [dx, dy] : directions) {
                 int newX = x + dx;
                 int newY = y + dy;
-                if (newX >= 0 && newX < image.size() && newY >= 0 && newY < image[0].size() && 
-                    image[newX][newY] == original) {
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n && image[newX][newY] == original) {
+                    image[newX][newY] = color;
                     q.push({newX, newY});
                 }
             }
