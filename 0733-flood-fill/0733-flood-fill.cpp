2a85620292b6599c32a8369b0817eb5898a4f40f
Chun Pei, Chen (Peggy) <86610644+PeggyChen19@users.noreply.github.com>
2025-06-30 17:21:07 +0800

    Time: 1 ms (22.67%), Space: 18.2 MB (49.82%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int original = image[sr][sc];
+        if (original == color) return image; // no need to change
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+            image[x][y] = color;
+            for (auto [dx, dy] : directions) {
+                int newX = x + dx;
+                int newY = y + dy;
+                if (newX >= 0 && newX < image.size() && newY >= 0 && newY < image[0].size() && 
+                    image[newX][newY] == original) {
+                    q.push({newX, newY});
+                }
+            }
+        }
+        return image;
+    }
+};
+/*
+Record the original color
+Do BFS / DFS to fill the neighbors with original color
+*/
