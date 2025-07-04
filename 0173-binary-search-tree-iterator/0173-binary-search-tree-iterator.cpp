c88705a3bab23471a51c607ed782745e39d59065
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-04 22:33:44 +0800

    Time: 7 ms (26.29%), Space: 31.9 MB (67.75%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -1,54 +1,36 @@
 class BSTIterator {
 public:
-    TreeNode* pointer;
-    vector<TreeNode*> path;
+    stack<TreeNode*> path;
 
     BSTIterator(TreeNode* root) {
-        pointer = root;
-        findLeftMost();
-        path.push_back(pointer);
-        pointer = nullptr;
+        findLeftMost(root);
     }
     
     int next() {
-        if (pointer && pointer->right) { // go right once and keep exploring left
-            pointer = pointer->right;
-            findLeftMost();
-        } else {
-            pointer = path.back();
-            path.pop_back();
+        TreeNode* node = path.top();
+        path.pop();
+        if (node->right) { // go right once and keep exploring left
+            findLeftMost(node->right);
         }
-        return pointer->val;
+        return node->val;
     }
     
     bool hasNext() {
-        if (pointer && pointer->right) {
-            return true;
-        } else {
-            return !path.empty();
-        }
+        return !path.empty();
     }
 
 private:
-    void findLeftMost() {
-        while (pointer->left) {
-            path.push_back(pointer);
-            pointer = pointer->left;
+    void findLeftMost(TreeNode* node) {
+        while (node) {
+            path.push(node);
+            node = node->left;
         }
     }
 };
 /*
-    5
-   / \
-  3   6
- / \   \
-1  4    7
- \
-  2
-members: pointer, path vector
+members: path stack 
 
-init: find the left most element and record the path
+init: find the left most node and record the path
 next: in-order: left -> root -> right
-if cur->right: go right once and keep exploring left
-else: go up until meeting an unvisited parent
+top is the next, try to explore the left most node from top->right
 */
