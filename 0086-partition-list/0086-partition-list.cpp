739adc268bcef7d0923017dbfc0d37ab902c8e46
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-05 00:20:32 +0800

    Time: 0 ms (100%), Space: 15 MB (46.03%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode *dummyLeftHead = new ListNode(), *dummyRightHead = new ListNode();
+        ListNode *curLeft = nullptr, *curRight = nullptr;
+        while (head) {
+            if (head->val < x) {
+                if (!curLeft) { // set up head
+                    curLeft = head;
+                    dummyLeftHead->next = curLeft;
+                } else {
+                    curLeft->next = head;
+                    curLeft = curLeft->next;
+                }
+            } else {
+                if (!curRight) { // set up head
+                    curRight = head;
+                    dummyRightHead->next = curRight;
+                } else {
+                    curRight->next = head;
+                    curRight = curRight->next;
+                }
+            }
+            head = head->next;
+        }
+        if (!curLeft) return dummyRightHead->next; // only right part
+        curLeft->next = dummyRightHead->next; // link two lists
+        if (curRight) { // ends at right
+            curRight->next = nullptr;
+        }
+        return dummyLeftHead->next;
+    }
+};
+/*
+ListNode pointers: dummyLeftHead, dummyRightHead, cur(head), curLeft, curRight
+Maintain these pointers
+*/
