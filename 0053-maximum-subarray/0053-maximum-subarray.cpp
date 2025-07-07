b0f305c258932194fedd2f342a339493ad4df612
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-08 00:22:33 +0800

    Time: 0 ms (100%), Space: 71.8 MB (52.12%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        int result = INT_MIN, minPrefixSum = 0;
-        vector<int> prefixSum(n + 1, 0);
-        for (int i = 1; i <= n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
-            result = max(result, prefixSum[i] - minPrefixSum);
-            minPrefixSum = min(minPrefixSum, prefixSum[i]);          
+        int result = INT_MIN, minPrefixSum = 0, curPrefixSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            curPrefixSum = curPrefixSum + nums[i];
+            result = max(result, curPrefixSum - minPrefixSum);
+            minPrefixSum = min(minPrefixSum, curPrefixSum);
         }
         return result;
     }
