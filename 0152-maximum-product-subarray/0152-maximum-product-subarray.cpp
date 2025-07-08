475ff6f2a1f694bf4dd0a06680228b4d7062d0b7
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-08 10:29:04 +0800

    Time: 0 ms (100%), Space: 17.7 MB (84.35%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int curMax = nums[0], curMin = nums[0], globalMax = nums[0];
+        int globalMax = nums[0], maxProduct = nums[0], minProduct = nums[0];
         for (int i = 1; i < nums.size(); i++) {
-            int a = curMax * nums[i];
-            int b = curMin * nums[i];
-            curMax = max({nums[i], a, b});
-            curMin = min({nums[i], a, b});
-            globalMax = max(globalMax, curMax);
+            int preMaxProduct = maxProduct, preMinProduct = minProduct;
+            maxProduct = max(nums[i], max(preMaxProduct * nums[i], preMinProduct * nums[i]));
+            minProduct = min(nums[i], min(preMaxProduct * nums[i], preMinProduct * nums[i]));
+            globalMax = max(globalMax, maxProduct);
         }
         return globalMax;
     }
 };
 /*
-subarray (continuous) -> Sliding Winodw or DP
-not Sliding Window, becasue we don't have a fixed condition to follow
-DP
-States: index, curMax, curMin
+Subarray -> DP / Sliding Window
+have negative nums -> not sliding window
 
-Transition:
-use global_max to record ans
-candidates = nums[i], curMax[i-1] * nums[i], curMin[i-1] * nums[i]
-curMax[i] = max(candidates)
-curMax[i] = min(candidates)
-
-Base case:
-curMax[0] = nums[0]
-curMin[0] = nums[0]
-
-Memorization:
-int curMax, curMin
+Subproblem: maxProduct & minProduct ends with index
+Transition function:
+maxProduct[i] = max(nums[i], maxProduct[i - 1] * nums[i], minProduct[i - 1] * nums[i])
+minProduct[i] = min(nums[i], maxProduct[i - 1] * nums[i], minProduct[i - 1] * nums[i])
 */
