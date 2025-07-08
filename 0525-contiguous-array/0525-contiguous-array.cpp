4315b9cd25271c46d7003ab35279f0d5674847b3
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-08 14:13:20 +0800

    Time: 28 ms (36.91%), Space: 88.3 MB (73.36%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        int maxLen = 0, curPrefixSum = 0;
-        unordered_map<int, int> prefixSum; // prefixSum, firstInd
-        prefixSum[0] = -1;
+        int result = 0, prefixSum = 0;
+        unordered_map<int, int> prefixSumInd; // prefixSum from 0 ~ i
+        prefixSumInd[0] = -1;
         for (int i = 0; i < nums.size(); i++) {
-            curPrefixSum += (nums[i] == 1) ? 1: -1;
-            if (prefixSum.count(curPrefixSum)) { // the prefixSum has existed
-                maxLen = max(maxLen, i - prefixSum[curPrefixSum]); // update maxLen
+            prefixSum += (nums[i] == 1 ? 1 : -1);
+            if (prefixSumInd.count(prefixSum)) {
+                result = max(result, i - prefixSumInd[prefixSum]);
             } else {
-                prefixSum[curPrefixSum] = i; // record the new prefixSum
+                prefixSumInd[prefixSum] = i;
             }
         }
-        return maxLen;
+        return result;
     }
 };
 /*
 Subarray -> Sliding Window / DP / Prefix Sum
-Not Sliding Window -> can't decide when to move left
-Not DP -> no optimal substructure -> 子問題的結果無法組合成大問題的解
-Target: find the max subarray where sum == 0 (replace 0 to -1)
-interval sum = curPrefixSum - prefixSum = 0
+Not Sliding Window -> can't decide to shrink or expand simply based on current states
 
-int maxLen = 0
-unordered_map<int, int> firstPrefixSum // prefixSum, firstInd
-go through nums
-    record curPrefixSum
-    find curprefixSum from firstPrefixSum
-    update firstPrefixSum
+Not DP -> no overlapping subproblems
+
+Prefix Sum:
+transfer to subarray sum problem by replacing 0 with -1
+try to find the longest subarray sums to 0
+
+int result = 0
+unordered_map<int, int> prefixSumInd // prefixSum, min ind where this sum occurred
+loop nums
+    calculate prefixSum (1 -> +1, 0 -> -1)
+    update result based on (i - prefixSumInd[prefixSum] + 1)
+    update prefixSumInd
 */
