32dbdddb769c6e5f477444d922be1f0a6ab31aab
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-08 14:18:37 +0800

    Time: 30 ms (29.58%), Space: 88.1 MB (92.28%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         int result = 0, prefixSum = 0;
-        unordered_map<int, int> prefixSumInd; // prefixSum from 0 ~ i
-        prefixSumInd[0] = -1;
+        unordered_map<int, int> firstPrefixSum; // prefixSum, first index where the sum occurred
+        firstPrefixSum[0] = -1;
         for (int i = 0; i < nums.size(); i++) {
             prefixSum += (nums[i] == 1 ? 1 : -1);
-            if (prefixSumInd.count(prefixSum)) {
-                result = max(result, i - prefixSumInd[prefixSum]);
+            if (firstPrefixSum.count(prefixSum)) {
+                result = max(result, i - firstPrefixSum[prefixSum]);
             } else {
-                prefixSumInd[prefixSum] = i;
+                firstPrefixSum[prefixSum] = i;
             }
         }
         return result;
@@ -18,17 +18,16 @@ public:
 /*
 Subarray -> Sliding Window / DP / Prefix Sum
 Not Sliding Window -> can't decide to shrink or expand simply based on current states
-
-Not DP -> no overlapping subproblems
+Not DP -> no optimal substructure -> 子問題的結果無法組合成大問題的解
 
 Prefix Sum:
 transfer to subarray sum problem by replacing 0 with -1
 try to find the longest subarray sums to 0
 
 int result = 0
-unordered_map<int, int> prefixSumInd // prefixSum, min ind where this sum occurred
+unordered_map<int, int> firstPrefixSum // prefixSum, first index where the sum occurred
 loop nums
     calculate prefixSum (1 -> +1, 0 -> -1)
-    update result based on (i - prefixSumInd[prefixSum] + 1)
-    update prefixSumInd
+    update result based on (i - firstPrefixSum[prefixSum])
+    update firstPrefixSum
 */
