436e2bc19fe6d5d76409ff764fe1695047572ab9
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-08 17:49:47 +0800

    Time: 0 ms (100%), Space: 8.5 MB (50.97%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        for (int i = 1; i <= x; i++) {
-            if (i > x / i) {
-                return i - 1;
+        if (x == 0 || x == 1) return x;
+        int left = 1, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= (long) x) { // go right
+                left = mid + 1;
+            } else { // go left
+                right = mid - 1;
             }
         }
-        return x;
+        // left is the first element which square > x
+        return left - 1;
     }
 };
 /*
 find square root of x rounded down to the nearest integer
-if (square root of x) is y -> x = y ** 2
-rounded_down_y ** 2 <= x
-(rounded_down_y + 1) ** 2 > x
+rounded_down_root ** 2 <= x
+(rounded_down_root + 1) ** 2 > x
+
+Improvement: use binary search
 */
