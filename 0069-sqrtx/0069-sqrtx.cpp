e70cd819e3ce2f4c323173a75853b6b53da4a73e
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-08 17:52:54 +0800

    Time: 0 ms (100%), Space: 8.5 MB (50.97%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -5,20 +5,20 @@ public:
         int left = 1, right = x;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if ((long) mid * mid <= (long) x) { // go right
+            if ((long) mid * mid <= x) { // go right
                 left = mid + 1;
             } else { // go left
                 right = mid - 1;
             }
         }
-        // left is the first element which square > x
+        // after loop, left is the first element which left^2 > x
         return left - 1;
     }
 };
 /*
 find square root of x rounded down to the nearest integer
-rounded_down_root ** 2 <= x
-(rounded_down_root + 1) ** 2 > x
+rounded_down_root ^ 2 <= x
+(rounded_down_root + 1) ^ 2 > x
 
 Improvement: use binary search
 */
