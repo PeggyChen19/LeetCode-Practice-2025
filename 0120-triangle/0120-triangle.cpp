de33b2b63c7d833107a022d1133413a60afa9e66
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-14 11:03:38 +0800

    Time: 0 ms (100%), Space: 12.5 MB (87.66%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int row = triangle.size();
+        if (row == 0) return 0;
+        if (row == 1) return triangle[0][0];
+
+        vector<int> dp(row, INT_MAX);
+        dp[0] = triangle[0][0];
+        for (int i = 1; i < row; i++) {
+            for (int j = i; j > 0; j--) {
+                dp[j] = min(dp[j - 1], dp[j]) + triangle[i][j];
+            }
+            dp[0] += triangle[i][0];
+        }
+
+        int minTotal = INT_MAX;
+        for (int i = 0; i < row; i++) {
+            minTotal = min(minTotal, dp[i]);
+        }
+        return minTotal;
+    }
+};
+/*
+shortest path -> DP
+for each cell, calculate the min total
+dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+return the min total from the last row
+*/
