72336514d4056210fe446a089cd89c882fa84548
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-15 17:15:38 +0800

    Time: 0 ms (100%), Space: 10.6 MB (67.67%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,44 +1,56 @@
 class Solution {
-private:
-    const vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int h = board.size(), w = board[0].size();
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                if (dfs(board, i, j, 0, word)) {
-                    return true;
+        int m = board.size(), n = board[0].size();
+        if (earlyStop(board, m, n, word)) return false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(board, i, j, word, 0, m, n)) {
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
-
 private:
-    bool dfs(vector<vector<char>>& board, int i, int j, int word_ind, const string& word) {
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[word_ind]) { 
-            return false; // out of range || not match || have visited
-        }
-        if (word_ind == word.size() - 1) { // finish matching
-            return true;
-        }
-        char tmp = board[i][j];
+    vector<pair<int, int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; 
+    bool dfs(vector<vector<char>>& board, int i, int j, string& word, int ind, int& m, int& n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[ind]) return false;
+        if (ind == word.size() - 1) return true;
         board[i][j] = '#';
-        for (int k = 0; k < directions.size(); k++) {
-            int x = i + directions[k].first;
-            int y = j + directions[k].second;
-            if (dfs(board, x, y, word_ind + 1, word)) {
-                return true;
+        for (auto& [dx, dy] : directions) {
+            int x = i + dx;
+            int y = j + dy;
+            if (dfs(board, x, y, word, ind + 1, m, n)) return true;
+        }
+        board[i][j] = word[ind];
+        return false;
+    }
+
+    bool earlyStop(vector<vector<char>>& board, int m, int n, string& word) {
+        vector<int> wordFreq(100, 0);
+        vector<int> boardFreq(100, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boardFreq[board[i][j] - 'A']++;
             }
         }
-        board[i][j] = tmp;
+        for (int i = 0; i < word.size(); i++) {
+            char c = word[i] - 'A';
+            wordFreq[c]++;
+            if (wordFreq[c] > boardFreq[c]) return true;
+        }
+        if (wordFreq[word[0] - 'A'] > wordFreq[word.back() - 'A']) reverse(word.begin(), word.end());
         return false;
     }
 };
 /*
-we need to enumerate all possibilities and use backtracking
-because the same letter cell may not be used more than once -> marked it
-for a node, it can go to top down left right 4 directions
-if there is no possible choices, go back the last step
+Try all possibilities: Backtracking(DFS)
+modify the board when visited, convert it back when backtracking
+
+Pruning method:
+1. Early return, compare word's char frequency v.s. board's char frequency
+2. Start from less frequent char (word.begin() or word.end())
 */
