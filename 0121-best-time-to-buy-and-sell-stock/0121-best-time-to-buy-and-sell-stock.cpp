921e1dbdf668c65cb0caa44ba1d243b9fe58500e
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-15 17:52:14 +0800

    Time: 0 ms (100%), Space: 97.3 MB (86.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int curMin = prices[0], curMax = prices[0];
+        int curMin = prices[0];
         int globalMax = 0;
         for (int& price : prices) {
-            if (price > curMax) {
-                curMax = price;
-                globalMax = max(globalMax, curMax - curMin);
-            }
-            if (price < curMin) {
-                curMin = price;
-                curMax = price;
-            }
+            globalMax = max(globalMax, price - curMin);
+            curMin = min(curMin, price);
         }
         return globalMax;
     }
 };
 /*
-go through the prices and record the current min & max
-use globalMax to store result
-when max is updated, calculate new profit
-when min is needed to update, set max to min
+go through the prices and record the curMin
+use globalMax to track result
+for all prices
+    update globalMax
+    update curMin
 */
