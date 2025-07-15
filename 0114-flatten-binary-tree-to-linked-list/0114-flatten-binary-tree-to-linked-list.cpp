1d76efb82661ab0e16ff4d149742a4b858b43c43
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-15 11:54:15 +0800

    Time: 1 ms (6.02%), Space: 17.7 MB (23.75%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    TreeNode* prev = nullptr;
     void flatten(TreeNode* root) {
-        if (!root) return;
-        flatten(root->right);
-        flatten(root->left);
-        root->right = prev;
-        root->left = nullptr;
-        prev = root;
+        TreeNode* prev = nullptr;
+        traverse(root, prev);
+    }
+
+private:
+    void traverse(TreeNode* node, TreeNode*& prev) {
+        if (!node) return;
+        traverse(node->right, prev);
+        traverse(node->left, prev);
+        node->right = prev;
+        node->left = nullptr;
+        prev = node;
     }
 };
 /*
@@ -15,8 +20,8 @@ preorder: root -> left -> right
 limit: in-place
 
 link all nodes via left and change all to right? hard to implement
-improvement: "reverse" preorder: right -> left -> root
-1. recursively traverse (right->left->root)
+improvement: "reverse" preorder: right -> left -> root, and use global pointer "prev"
+1. recursively traverse (right -> left -> root)
 2. cur->right = previous visited node
 3. cur->left = nullptr
 4. update prev
