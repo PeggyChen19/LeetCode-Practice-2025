045183e21c6d5659ea7ecba5647c3b4500f261d4
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-16 23:34:23 +0800

    Time: 3 ms (76.91%), Space: 14.2 MB (33.97%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion-extra-vector.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        vector<string> zigzag(numRows, "");
+        int i = 0;
+        bool goDown = false;
+        for (char& ch: s) {
+            zigzag[i].push_back(ch);
+            if (i == 0 || i == numRows - 1) goDown = !goDown;
+            i += goDown ? 1 : -1;
+        }
+        string result = "";
+        for (string& row : zigzag) result += row;
+        return result;
+    }
+};
+/*
+1     7
+2   6 8
+3 5   9
+4
+
+1. use vector<string> to record each row
+2. concatenate the vector to get the answer
+*/
