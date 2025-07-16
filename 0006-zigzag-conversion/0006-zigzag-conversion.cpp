0f9d6cd0142111245e489e9f855bbd5eb6b49c4f
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-16 23:34:08 +0800

    Time: 4 ms (64.38%), Space: 14.2 MB (33.97%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -2,37 +2,25 @@ class Solution {
 public:
     string convert(string s, int numRows) {
         if (numRows == 1) return s;
-        string result = "";
-        int n = s.size();
-        for (int i = 0; i < numRows; i++) {
-            int j = i;
-            while (j < n) {
-                result += s[j];
-                int mid = j + (numRows - 1 - i) * 2;
-                if (i != 0 && i != numRows - 1 && mid < n) {
-                    result += s[mid];
-                }
-                j += (numRows - 1) * 2;
-            }
+        vector<string> zigzag(numRows, "");
+        int i = 0;
+        bool goDown = false;
+        for (char& ch: s) {
+            zigzag[i].push_back(ch);
+            if (i == 0 || i == numRows - 1) goDown = !goDown;
+            i += goDown ? 1 : -1;
         }
+        string result = "";
+        for (string& row : zigzag) result += row;
         return result;
     }
 };
 /*
-0     6
-1   5 7
-2 4   8
-3     9
-  ^^^ ^
-  mid col
+1     7
+2   6 8
+3 5   9
+4
 
-Brute force: use an additional vector<vector<char>>, size: numRows
-Improvement: try to derive the formula
-for each row i:
-1. start with i (j = i) 
-2. result += j
-3. mid = j + (numRows - 1 - i) + (numRows - 1 - i)
-4. if middile row: result += mid
-5. i += numRows + numRows - 2
-6. loop 2~5
+1. use vector<string> to record each row
+2. concatenate the vector to get the answer
 */
