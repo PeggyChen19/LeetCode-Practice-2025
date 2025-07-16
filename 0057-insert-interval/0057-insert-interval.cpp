4005487f727764ec5a2552291de494036800d7fc
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-16 10:44:43 +0800

    Time: 0 ms (100%), Space: 22.2 MB (8.95%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> ans;
-        int i = 0;
-        int n = intervals.size();
-        while (i < n && intervals[i][1] < newInterval[0]) { // before new
-            ans.push_back(intervals[i]);
-            i++;
+        vector<vector<int>> results;
+        bool inserted = false;
+        for (vector<int> interval : intervals) {
+            if (inserted || interval[1] < newInterval[0]) {
+                results.push_back(interval);
+            }
+            else if (newInterval[1] < interval[0]) {
+                results.push_back(newInterval);
+                results.push_back(interval);
+                inserted = true;
+            } else {
+                newInterval[0] = min(newInterval[0], interval[0]);
+                newInterval[1] = max(newInterval[1], interval[1]);
+            }
         }
-        while (i < n && newInterval[1] >= intervals[i][0]) { // overlapping
-            newInterval[0] = min(intervals[i][0], newInterval[0]);
-            newInterval[1] = max(intervals[i][1], newInterval[1]);
-            i++;
+        if (!inserted) {
+            results.push_back(newInterval);
         }
-        ans.push_back(newInterval);
-        while (i < n) { // after new
-            ans.push_back(intervals[i]);
-            i++;
-        }
-        return ans;
+        return results;
     }
 };
-
 /*
-[1, 3], [7, 9]
-New: [4, 5] -> [1, 3], [4, 5], [7, 9]
-New: [2, 5] -> [1, 5], [7, 9]
-New: [2, 10] -> [1, 10]
-
-每個 interval 一定只屬於以下三類之一
-1. 在 new 前，完全沒重疊 -> 直接放入 ans
-2. 重疊 -> 開頭取 min，結尾取 max
-3. 在 new 後，完全沒重疊的 -> 直接放入 ans
-比較困難的點是怎麼去判斷，第一種是 i.end < new.start 很明顯，所以第一種結束的時候 i.end >= new.start，所以這兩個變數就不用再考慮了，接下來直接去想怎麼樣才會「重疊」很不直覺，所以可以先去想在 new 後的要怎麼樣才會「不重疊」，得到 new.end < i.start，所以反過來就會是重疊的條件
--> insight：先去想「不重疊」的條件，再去反推「重疊」的條件，不重疊又分為在前面或在後面
-
-For intervals before new (i.end < new.start) -> insert to ans directly
-For intervals overlap new (new.end >= i.start) -> merge
-For intervals after new (new.end < i.start) -> insert to ans directly
+intervals are non-overlapping -> sort both by start and end
+go through all i in intervals
+    if i.end < new.start // non-overlap and i before new
+        insert i
+    else if new.end < i.start // non-overlap and new before i
+        insert new
+        insert i
+    else // overlap -> update new
+        combine i into new
 */
