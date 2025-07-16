625ab199877d6e8b47d15e8b7dc8e236867f32c5
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-16 11:13:19 +0800

    Time: 1 ms (42.2%), Space: 17.1 MB (71.52%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,44 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        queue<TreeNode*> bfs_q;
-        if (!root) {
-            return ans;
-        }
-        bfs_q.push(root);
-        while (!bfs_q.empty()) {
-            int level_size = bfs_q.size();
-            vector<int> level;
-            for (int i = 0; i < level_size; i++) { // record current level and put all next-level nodes
-                TreeNode* node = bfs_q.front();
-                bfs_q.pop();
-                level.push_back(node->val);
-                if (node->left) {
-                    bfs_q.push(node->left);
-                }
-                if (node->right) {
-                    bfs_q.push(node->right);
-                }
+        if (!root) return {};
+        vector<vector<int>> result;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> levelContent(levelSize);
+            for (int i = 0; i < levelSize; i++) { // handle nodes level by level
+                TreeNode* node = q.front();
+                q.pop();
+                levelContent[i] = node->val;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
             }
-            ans.push_back(level);
+            result.push_back(levelContent);
         }
-        return ans;
+        return result;
     }
 };
-
 /*
-level by level -> BFS (queue)
-for diff level, diff vector -> record the current queue size (= level size) before insert the next level
+level by level -> BFS with queue
 */
