83dbec31beb7c9ee35457e2e16a4cb875130dca7
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-21 11:00:51 +0800

    Time: 32 ms (28.62%), Space: 23.3 MB (32.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,45 +1,40 @@
 class Solution {
-private:
-    const vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
+        int result = 0;
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == '1') { // starting point of land
-                    bfs(grid, i, j);
-                    ans++;
+                if (grid[i][j] == '1') {
+                   bfs(grid, i, j);
+                   result++;
                 }
             }
         }
-        return ans;
+        return result;
     }
-
 private:
+    vector<pair<int, int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    
     void bfs(vector<vector<char>>& grid, int i, int j) {
         queue<pair<int, int>> q;
         q.push({i, j});
-        grid[i][j] = '0';
+        grid[i][j] = '0'; 
         while (!q.empty()) {
-            i = q.front().first;
-            j = q.front().second;    
+            auto [x, y] = q.front();
             q.pop();
-            for (auto [dx, dy] : directions) {
-                int x = i + dx;
-                int y = j + dy;
-                if (x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size() && grid[x][y] == '1') {
-                    q.push({x, y});
-                    grid[x][y] = '0';
+            for (auto& [dx, dy] : directions) {
+                int a = x + dx;
+                int b = y + dy;
+                if (a >= 0 && a < grid.size() && b >= 0 && b < grid[0].size() && grid[a][b] == '1') {
+                    q.push({a, b});
+                    grid[a][b] = '0';
                 }
             }
         }
     }
 };
-
 /*
-loop the whole grid
-    find the starting point land
-    expand from 4 directions and marked it as "0" (visited) until reach water (DFS)
-T: O(m*n)
+DFS / BFS
+After visiting -> set to 0 (water)
+go through the matrix, explore every island, return the count
 */
