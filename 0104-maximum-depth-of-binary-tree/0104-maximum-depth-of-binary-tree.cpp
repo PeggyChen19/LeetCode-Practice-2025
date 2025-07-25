c8505e13a8e5222184d11ed96b132072d8c73298
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-25 18:14:19 +0800

    Time: 0 ms (100%), Space: 18.9 MB (76.01%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -1,34 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        int depth = 0;
-        return dfs(root, depth);
-    }
-    int dfs(TreeNode* root, int depth) {
-        if (root) {
-            depth++;
-            return max( dfs(root->left, depth), dfs(root->right, depth) );
-        } else {
-            return depth;
+        if (!root) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* cur = q.front();
+                q.pop();
+                if(cur->left) q.push(cur->left);
+                if(cur->right) q.push(cur->right);
+            }
+            level++;
         }
-    };
+        return level;
+    }
 };
-
-/*
-maximum depth -> the farest node -> DFS
-use recursion to achieve it
-depth adds one if root is not null
-recur left
-recur right
-*/
