9987620345d152c4df82ec2698277e21b61cd296
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-25 18:15:54 +0800

    Time: 0 ms (100%), Space: 19.1 MB (45.92%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree-dfs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) {
+            return 0;
+        } else {
+            return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        }
+    }
+};
+
+/*
+maximum depth -> the farest node -> DFS
+use recursion to achieve it
+*/
