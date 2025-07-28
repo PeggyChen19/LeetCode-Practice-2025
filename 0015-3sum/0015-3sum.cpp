56b70acdffce6171042459ea69ba3c2a8030f08c
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-28 11:17:40 +0800

    Time: 50 ms (55.13%), Space: 28.9 MB (99.04%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
+        int size = nums.size();
+        vector<vector<int>> result;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size() - 2; i++) { // the last possible ans: s-3, s-2, s-1
-            if (i - 1 >= 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int left = i + 1;
-            int right = nums.size() - 1;
-            while (left < right) { // left must not be equal to right 
+        for (int i = 0; i < size - 2; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) continue;
+            int left = i + 1, right = size - 1;
+            while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
-                    ans.push_back({nums[i], nums[left], nums[right]});
-                    while (left + 1 < nums.size() && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    while (right - 1 >= 0 && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
-                    left++;
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    do left++; while (left < right && nums[left - 1] == nums[left]);
+                    do right--; while (left < right && nums[right + 1] == nums[right]);
+                } else if (sum > 0) { // be smaller
                     right--;
-                } else if (sum < 0) {
+                } else { // be bigger
                     left++;
-                } else {
-                    right--;
                 }
             }
         }
-        return ans;
+        return result;
     }
 };
-
 /*
-sort the array -> O(nlogn)
-set a starting point to be the fixed value and go through the array 
-    set left(fixed + 1) and right(end) pointers
-    based on the sum of left & right to adjust left and right
-    store the new answers
-return all answers
+fix one element, find the remaining two -> time complexity: O(n * ?)
+for finding the remaining two, our target is O(n) -> two pointers -> sort the nums
+Overall time complexity: O(n^2) + O(nlogn) = O(n^2)
+Note: no duplicated result, every num can be selected once only
 */
