dd833533ea9d3d2a30608c8d7afa22ed2ad5953a
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-07-28 10:05:13 +0800

    Time: 9 ms (51.47%), Space: 19.3 MB (95.18%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        int size = 1, prev = nums[0];
-        bool twice = false;
-        for (int i = 1; i < nums.size(); i++) {
-            if (prev == nums[i]) { // duplicate
-                if (!twice) { // valid
-                    twice = true;
-                    swap(nums[size], nums[i]);
-                    size++;
-                }
-            } else { // update prev
-                prev = nums[i];
-                twice = false;
-                swap(nums[size], nums[i]);
+        int size = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (size < 2 || nums[size - 2] != nums[i]) {
+                nums[size] = nums[i];
                 size++;
             }
         }
@@ -25,5 +15,5 @@ public:
 Target: keep at most two the same elements
 Method: 
 1. keep one pointer (int) points to the next element of the end of valid list
-2. when there are more than two elements -> points to the redundant and wait for swapping with the next valid element 
+2. when there are more than two elements -> wait for swapping with the next valid element 
 */
