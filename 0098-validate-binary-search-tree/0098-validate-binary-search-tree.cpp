495f5ecdd04f27c046d0b3dabc99ef5e61d793f9
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-08-07 00:10:07 +0800

    Time: 0 ms (100%), Space: 21.8 MB (94.68%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,36 +1,12 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        if (!root) {
-            return true;
-        }
-        return ( valid(root->left, LONG_MIN, root->val) && valid(root->right, root->val, LONG_MAX) );
+        return valid(root, LONG_MIN, LONG_MAX);        
     }
 private:
-    bool valid(TreeNode* node, long min, long max) {
-        if (!node) {
-            return true;
-        }
-        if (node->val > min && node->val < max) { // valid for the current node -> go to left and right sub nodes
-            return valid(node->left, min, node->val) && valid(node->right, node->val , max);
-        } else {
-            return false;
-        }
+    bool valid(TreeNode* node, long minVal, long maxVal) {
+        if (!node) return true;
+        if (node->val <= minVal || node->val >= maxVal) return false;
+        return valid(node->left, minVal, node->val) && valid(node->right, node->val, maxVal);
     }
 };
-
-/*
-recursively check the whole tree
-use min & max range to control the values in subtrees
-*/
