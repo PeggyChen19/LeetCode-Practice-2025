2e94e63c8cf409203e4fdfd6fc20ac07cc49e40c
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-01 06:57:14 +0800

    Time: 0 ms (100%), Space: 7.9 MB (59.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
-        } else if (n == 2) {
-            return 2;
+        int pre1 = 1, pre2 = 0;
+        for (int i = 1; i <= n; i++) {
+            int cur = pre1 + pre2;
+            pre2 = pre1;
+            pre1 = cur;
         }
-        int back_two = 1;
-        int back_one = 2;
-        for (int i = 3; i <= n; i++) {
-            int tmp = back_one;
-            back_one = back_two + back_one;
-            back_two = tmp;
-        }
-        return back_one;
+        return pre1;
     }
 };
-
 /*
-to reach n: from n-1 and then go 1 OR from n-2 and then go 2
-f(n) = f(n-1) + f(n-2)
-f(1) = 1
-f(2) = 2
-we can actually only record two values f(n-1) & f(n-2)
-use a loop to go through all stairs
+the i step ways = (i - 1) + (i - 2)
 */
