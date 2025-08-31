ebc4a48d4e0a44c49401f44e4c6d3ff8974e1ecd
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-01 00:55:57 +0800

    Time: 0 ms (100%), Space: 11.8 MB (74.56%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        vector<double> result;
+        unordered_map<string, vector<pair<string, double>>> adjacent;
+        for (int i = 0; i < equations.size(); i++) {
+            adjacent[equations[i][0]].push_back({equations[i][1], values[i]});
+            adjacent[equations[i][1]].push_back({equations[i][0], (1 / values[i])});
+        }
+        for (int i = 0; i < queries.size(); i++) {
+            unordered_set<string> visited;
+            if (!adjacent.count(queries[i][0]) || !adjacent.count(queries[i][1])) result.push_back(-1);
+            else if (queries[i][0] == queries[i][1]) result.push_back(1);
+            else {
+                double ans = -1;
+                queue<pair<string, double>> q;
+                q.push({queries[i][0], 1});
+                visited.insert(queries[i][0]);
+                while (!q.empty()) {
+                    auto [n, w] = q.front();
+                    q.pop();
+                    for (auto& [neighbor, weight] : adjacent[n]) {
+                        if (!visited.count(neighbor)) {
+                            q.push({neighbor, w * weight});
+                            visited.insert(neighbor);
+                            if (neighbor == queries[i][1]) {
+                                ans = w * weight;
+                                break;
+                            }
+                        }
+                    }
+                }
+                result.push_back(ans);
+            }
+        }
+        return result;
+    }
+};
+
+/*
+looks like a math question, but we can solve it in a programming way!
+given
+1. "pairwise" relations between symbols
+2. a new relation can be derived from existing multiple relations
+--> GRAPH!
+symbols -> nodes
+relations -> weights
+A/B * B/C to eliminate B -> a path from A to C
+
+1. Build a graph (adjacent list): A -> B (val) & B -> A (1/val)
+2. Create a visited list
+3. BFS
+*/
