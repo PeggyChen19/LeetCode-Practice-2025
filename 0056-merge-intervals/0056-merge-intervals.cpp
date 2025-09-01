053581dfeddbb79e27c31068261853eafe827943
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-01 15:02:40 -0500

    Time: 3 ms (89.48%), Space: 23.9 MB (57.23%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) {
-            return intervals;
-        }
+        if (intervals.empty()) return intervals;
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> ans = {intervals[0]};
+        vector<vector<int>> result;
+        vector<int> current = intervals[0];
         for (int i = 1; i < intervals.size(); i++) {
-            if (ans.back()[1] < intervals[i][0]) { // non-overlapping
-                ans.push_back(intervals[i]);
-            } else { // overlapping -> update ans.back()[1]
-                // don't need to update start because intervals are sorted
-                ans.back()[1] = max(intervals[i][1], ans.back()[1]);
+            if (current[1] >= intervals[i][0]) { // overlap
+                current = {min(current[0], intervals[i][0]), max(current[1], intervals[i][1])};
+            } else {
+                result.push_back(current);
+                current = intervals[i];
             }
         }
-        return ans;
+        result.push_back(current);
+        return result;
     }
 };
-
 /*
-sort intervals
-go through all intervals except the last one
-    if (i).end < (i+1).start // non-overlapping
-        append(i)
-    else
-        merge 2 intervals (star = min, end = max)
-        store merged interval to i+1
-insert the last one
+sort the intervals (by start)
+check the overlap (interval[i][1] >= interval[i + 1][0]) -> merge
+current interval = the first interval
+for each interval
+    overlap -> merge
+    not overlap
+        push current into answer
+        change current to the new not overlap interval
 */
