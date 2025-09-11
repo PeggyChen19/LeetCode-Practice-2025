32fbb02eb9aa4bd01bc931f94eedc40522801fd9
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-11 17:57:50 -0500

    Time: 3 ms (2.93%), Space: 11.7 MB (12.11%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zero = 0, cur = 0, two = nums.size() - 1;
-        while (cur <= two) {
-            if (nums[cur] == 0) {
-                swap(nums[cur], nums[zero]);
-                zero++;
-                cur++; // cur == 0 or 1 (not 2) -> safe -> skip it
-            } else if (nums[cur] == 2) {
-                swap(nums[cur], nums[two]);
-                two--;
-                // cur == 0 or 1 or 2 -> cannot skip -> handle it next round
-            } else {
-                cur++;
+        unordered_map<int, int> count = {{0, 0}, {1, 0}, {2, 0}};
+        for (int i = 0; i < nums.size(); i++) {
+            count[nums[i]]++;
+        }
+        int i = 0;
+        for (int color = 0; color < 3; color++) {
+            for (int j = 0; j < count[color]; j++) {
+                nums[i] = color;
+                i++;
             }
         }
     }
 };
-/*
-Two pointers: zero = 0, two = size - 1
-iterate cur from zero to two
-if i == 0
-    swap; zero++; i++;
-else if i == 2
-    swap; two--;
-else
-    cur++;
-Dutch National Flag Algorithm
-只有從 right 換進來的數值要再檢查一次，從 left 換來的值是安全的
-*/
