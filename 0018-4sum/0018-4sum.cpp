937c0a9997c09d6ad59fa8cc20cbb01617535d09
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-12 16:38:12 -0500

    Time: 31 ms (17.55%), Space: 17.3 MB (83.58%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = 0; i <= n - 4; i++) {
+            if (i != 0 && nums[i - 1] == nums[i]) continue;
+            for (int j = i + 1; j <= n - 3; j++) {
+                if (j != i + 1 && nums[j - 1] == nums[j]) continue;
+                int l = j + 1, r = n - 1;
+                while (l < r) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[j], nums[l], nums[r]});
+                        do {l++;} while (l < r && nums[l - 1] == nums[l]);
+                        do {r--;} while (l < r && nums[r + 1] == nums[r]);
+                    } else if (sum > target) {
+                        do {r--;} while (l < r && nums[r + 1] == nums[r]);
+                    } else {
+                        do {l++;} while (l < r && nums[l - 1] == nums[l]);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
+/*
+2 sum: unordered_map O(n)
+3 sum: sort + two pointers O(n^2)
+4 sum: sort + double loop + two pointers O(n^3)
+*/
