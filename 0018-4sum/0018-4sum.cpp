41bd6b6cf3901aaec99a059897f730b2e07729f7
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-12 17:04:04 -0500

    Time: 1 ms (98.63%), Space: 17.3 MB (83.58%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> result;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
+        if (n < 4) return result;
+        sort(nums.begin(), nums.end());
         for (int i = 0; i <= n - 4; i++) {
             if (i != 0 && nums[i - 1] == nums[i]) continue;
+
+            // pruning
+            if ((long long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            if ((long long) nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) continue; // need larger i
+
             for (int j = i + 1; j <= n - 3; j++) {
                 if (j != i + 1 && nums[j - 1] == nums[j]) continue;
                 long long remain = (long long) target - nums[i] - nums[j];
+
+                // pruing
+                if ((long long)nums[j + 1] + nums[j + 2] > remain) break;
+                if ((long long)nums[n - 2] + nums[n - 1] < remain) continue; // need larger remain
+
                 int l = j + 1, r = n - 1;
                 while (l < r) {
                     long long twoSum = (long long) nums[l] + nums[r];
