092226071451578566c94791f133960ff89eb656
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-12 16:59:01 -0500

    Time: 23 ms (54.46%), Space: 17.3 MB (83.58%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -8,14 +8,15 @@ public:
             if (i != 0 && nums[i - 1] == nums[i]) continue;
             for (int j = i + 1; j <= n - 3; j++) {
                 if (j != i + 1 && nums[j - 1] == nums[j]) continue;
+                long long remain = (long long) target - nums[i] - nums[j];
                 int l = j + 1, r = n - 1;
                 while (l < r) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[l] + nums[r];
-                    if (sum == target) {
+                    long long twoSum = (long long) nums[l] + nums[r];
+                    if (twoSum == remain) {
                         result.push_back({nums[i], nums[j], nums[l], nums[r]});
                         do {l++;} while (l < r && nums[l - 1] == nums[l]);
                         do {r--;} while (l < r && nums[r + 1] == nums[r]);
-                    } else if (sum > target) {
+                    } else if (twoSum > remain) {
                         do {r--;} while (l < r && nums[r + 1] == nums[r]);
                     } else {
                         do {l++;} while (l < r && nums[l - 1] == nums[l]);
