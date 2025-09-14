0b6c2b8b2830633155733f28c810b816cf90d9a5
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-14 12:33:38 -0500

    Time: 2 ms (70.61%), Space: 23.1 MB (11.91%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -1,19 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return buildBST(nums, 0, nums.size() - 1);
+        if (nums.empty()) return nullptr;
+        return buildTree(nums, 0, nums.size() - 1);
     }
 private:
-    TreeNode* buildBST(vector<int>& nums, int left, int right) {
-        if (left > right) return nullptr;
-        int mid = left + (right - left) / 2;
-        TreeNode* root = new TreeNode(nums[mid]);
-        root->left = buildBST(nums, left, mid - 1);
-        root->right = buildBST(nums, mid + 1, right);
-        return root;
+    TreeNode* buildTree(vector<int>& nums, int start, int end) {
+        if (start > end) return nullptr;
+        if (start == end) return new TreeNode(nums[start]);
+        int mid = start + (end - start) / 2;
+        TreeNode* center = new TreeNode(nums[mid], buildTree(nums, start, mid - 1), buildTree(nums, mid + 1, end));
+        return center;
     }
 };
 /*
-always pick the mid as the node of current layer
-seperate into left part and right part, keep building the tree
+height-balanced -> nearly the same numbers of nodes in left & right trees
+recursion
+    find the center
+    if center is leaf -> return center
+    center->left = recursion(start ~ center-1)
+    center->right = recursion(center+1 ~ end)
+    return center
 */
