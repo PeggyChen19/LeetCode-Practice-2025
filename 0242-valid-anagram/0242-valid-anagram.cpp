0206f7b659d313b1c576a524fefec471c4bbe2a2
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-16 15:35:45 -0500

    Time: 0 ms (100%), Space: 9.7 MB (74.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int frequent[26] = {0};
         if (s.size() != t.size()) {
             return false;
         }
+        vector<int> frequent(26, 0);
         for (int i = 0; i < s.size(); i++) {
             frequent[s[i]-'a']++;
         }
@@ -19,26 +19,3 @@ public:
         return true;
     }
 };
-
-/*
-Solution 1:
-1. sort both string and store in a vector
-2. compare from the first to the last
-T: O(nlogn)
-S: O(n)
-
-Solution 2: -> better
-1. for the first string, store in an unordred set
-2. go through the second string, and find the character in the set. if find it, then remove it, if not, return false
-3. check if the set is empty
-T: O(n)
-S: O(n)
-
-Solution 3: -> best
-Just simply use vector to record it
-
-T: O(n)
-S: O(n)
-
-
-*/
