d3d3c3a5d2d0fb5107c3af12c62b9ffcd6e59340
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-18 22:42:42 -0500

    Time: 12 ms (44.05%), Space: 14.3 MB (21.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {    
-        int globalMax = 0, left = 0;
+    int lengthOfLongestSubstring(string s) {
+        int globalMax = 0;
+        int left = 0;
         unordered_set<char> window;
         for (int right = 0; right < s.size(); right++) {
-            char newChar = s[right];
-            while (window.count(newChar)) { // shrink until no newChar in current window
+            while (window.count(s[right])) { // remove duplication and move window forward
                 window.erase(s[left]);
                 left++;
-            }            
-            window.insert(newChar); // insert new char
+            }
+            window.insert(s[right]); // insert new element into window
             globalMax = max(globalMax, right - left + 1);
         }
         return globalMax;
     }
 };
-/*
-Slding window
-use globalMax to track the result
-use unordered_set to check duplicate
-while not reach end
-    expand right until duplicated
-    shrink left until not duplicated
-*/
