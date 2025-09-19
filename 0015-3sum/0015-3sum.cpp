a2bc5876659cad32fe40d662a6a6c79fd55f5a73
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-18 22:52:03 -0500

    Time: 51 ms (50.22%), Space: 29.2 MB (48.08%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int size = nums.size();
         vector<vector<int>> result;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < size - 2; i++) {
-            if (i > 0 && nums[i - 1] == nums[i]) continue;
-            int left = i + 1, right = size - 1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1, right = nums.size() - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
                     result.push_back({nums[i], nums[left], nums[right]});
-                    do left++; while (left < right && nums[left - 1] == nums[left]);
-                    do right--; while (left < right && nums[right + 1] == nums[right]);
-                } else if (sum > 0) { // be smaller
-                    right--;
-                } else { // be bigger
-                    left++;
+                    do left++; while (left < right && nums[left] == nums[left - 1]);
+                    do right--; while (left < right && nums[right] == nums[right + 1]);
+                } else if (sum < 0) {
+                    do left++; while (left < right && nums[left] == nums[left - 1]);
+                } else {
+                    do right--; while (left < right && nums[right] == nums[right + 1]);
                 }
             }
         }
         return result;
     }
 };
-/*
-fix one element, find the remaining two -> time complexity: O(n * ?)
-for finding the remaining two, our target is O(n) -> two pointers -> sort the nums
-Overall time complexity: O(n^2) + O(nlogn) = O(n^2)
-Note: no duplicated result, every num can be selected once only
-*/
