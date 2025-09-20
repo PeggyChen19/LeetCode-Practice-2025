dd4faaf58e046b157046e5ed4dff33cebc1e320f
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-20 11:52:02 -0500

    Time: 173 ms (64.28%), Space: 12.5 MB (90.97%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,21 +1,34 @@
-// the smallest prime: 2
-
 class Solution {
 public:
     int countPrimes(int n) {
-        int ans = 0;
-        vector<bool> isPrime(n + 1, true);
-        for(int i = 2; i < n; i++){
-            if(isPrime[i] == true){
-                ans++;
-                int cur = i;
-                while((long)cur * i < (long)n){ 
-                    isPrime[cur * i] = false;
-                    cur++;
+        if (n <= 2) return 0;
+        vector<bool> primes(n, true);
+        primes[0] = false;
+        primes[1] = false;
+        for (int i = 0; i * i < n; i++) { // go through all nums
+            if (primes[i]) {
+                for (int multiple = i * i; multiple < n; multiple += i) {
+                    primes[multiple] = false;
                 }
             }
-            else continue;
         }
-        return ans;
+        int count = 0;
+        for (bool prime : primes) {
+            if (prime) count++;
+        }
+        return count;
     }
 };
+/*
+if n < 2: return 0
+create bool vector, mark all nums to be true (will mark them as false via multiplying primes later)
+mark 0, 1 as false
+iterate i from 1 to n
+    if prime[i] = true
+        // i * 0 ~ i * (i - 1) have handled by other smaller prime
+        iterate multiple from i * i to n, multiple += i
+            prime[multiple] = false
+calculate prime counts
+
+optimal (optional): skip even nums
+*/
