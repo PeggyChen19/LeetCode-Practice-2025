02f1b469a8d1dc90ba667c72b1bb6005165a0798
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-21 16:57:10 -0500

    Time: 0 ms (100%), Space: 11.4 MB (97.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int, int> count = {{0, 0}, {1, 0}, {2, 0}};
-        for (int i = 0; i < nums.size(); i++) {
-            count[nums[i]]++;
-        }
-        int i = 0;
-        for (int color = 0; color < 3; color++) {
-            for (int j = 0; j < count[color]; j++) {
-                nums[i] = color;
-                i++;
+        int zero = 0, cur = 0, two = nums.size() - 1;
+        while (cur <= two) {
+            if (nums[cur] == 0) {
+                swap(nums[zero], nums[cur]);
+                zero++;
+                cur++;
+            } else if (nums[cur] == 2) {
+                swap(nums[two], nums[cur]);
+                two--;
+            } else {
+                cur++;
             }
         }
     }
 };
+/*
+3 parts: [0][1][2] -> use pointers to track the boundaries of the parts
+0 0 0 x x x 1 1 x x 2 2 
+      z           t
+pointers, zero = 0 (the next position can fill with 0), two = n - 1, cur
+use cur pointer to iterate the nums
+cur = 0
+while (cur <= two)
+    if nums[cur] == 0
+        swap(nums[zero], nums[cur])
+        cur++ // zero must have 0 or 1, which is valid
+        zero++
+    elif nums[cur] == 2
+        swap(nums[cur], nums[two])
+        two--;
+        // check cur next round
+    else
+        cur++
+    
+*/
