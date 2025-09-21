e07a72437af62af4230acdc0b21ce800d963961e
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-21 18:26:45 -0500

    Time: 3 ms (4.91%), Space: 8 MB (11.02%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n == 0) return false;
+        vector<int> basicPrimes = {2, 3, 5};
+        for (int prime : basicPrimes) {
+            while (n % prime == 0) {
+                n /= prime;
+            }
+            if (n == 1) return true;
+        }
+        return n == 1; 
+    }
+};
+/*
+ugly number n = 2^a * 3^b * 5^c
+brute force:
+try to dived n with 2, 3, 5 until it has remainder (not ugly) or it reaches 1 (ugly)
+*/
