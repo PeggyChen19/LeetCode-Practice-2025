c03ee3d1bc510cd4a78985ae1547cf3336141040
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-20 23:40:05 -0500

    Time: 0 ms (100%), Space: 19.5 MB (29.22%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,23 +11,35 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummyHead;
-        ListNode* newCur = &dummyHead;
-        while (list1 && list2) {
-            if (list1->val < list2->val) {
-                newCur->next = list1;
-                list1 = list1->next;
+        ListNode* dummyHead = new ListNode();
+        ListNode* curNew = dummyHead;
+        ListNode* cur1 = list1;
+        ListNode* cur2 = list2;
+        while (cur1 && cur2) {
+            if (cur1->val < cur2->val) { // choose cur1 (smaller one) and move forward
+                curNew->next = cur1;
+                cur1 = cur1->next;
             } else {
-                newCur->next = list2;
-                list2 = list2->next;
+                curNew->next = cur2;
+                cur2 = cur2->next;
             }
-            newCur = newCur->next;
+            curNew = curNew->next; // move new list forward
         }
-        if (list1) {
-            newCur->next = list1;
-        } else if (list2) {
-            newCur->next = list2;
+        if (cur1) { // cur2 is finished but cur1 is not, so connect the remaining cur1 to new list
+            curNew->next = cur1;
+        } else if (cur2) {
+            curNew->next = cur2;
         }
-        return dummyHead.next;
+        return dummyHead->next;
     }
 };
+/*
+curNew = dummyHead
+cur1 = list1->head;
+cur2 = list2->head;
+while (cur1 && cur2)
+    compare cur1 and cur2
+    curNew->next = the smaller one
+    move the smaller pointer forward
+finish the remaining of list1 or 2 if exists
+*/
