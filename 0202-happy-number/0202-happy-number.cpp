68b4ec6c07d19b5e46f0fd249b143ad96f0cb277
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-21 17:25:34 -0500

    Time: 0 ms (100%), Space: 7.9 MB (72.97%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,25 +2,43 @@ class Solution {
 public:
     bool isHappy(int n) {
         int fast = n, slow = n;
-        do {
-            fast = square(square(fast));
-            slow = square(slow);
-        } while (fast != slow);
-
-        return slow == 1;
-    }   
-private:
-    int square(int num) {
-        int result = 0;
-        while (num) {
-            result += (num % 10) * (num % 10);
-            num /= 10;
+        while (true) {
+            fast = process(process(fast));
+            slow = process(slow);
+            if (fast == 1) return true;
+            if (fast == slow) return false;
         }
-        return result;
+    }
+private:
+    int process(int n) {
+        int newN = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            newN += digit * digit;
+            n /= 10;
+        }
+        return newN;
     }
 };
 /*
-How to know if we enter a cycle? 
-Do we need to record all the numbers?
-No -> we can use fast & slow pointers to detect cycle
+we need to detect the cycle except "1" -> fast & slow pointers (will meet each other if there is a cycle)
+helper function:
+int process(int n)
+    int newN = 0;
+    while (n > 0)
+        digit = n % 10
+        newN += digit * digit
+        n /= 10
+    return newN
+
+fast = n
+slow = n
+while (true)
+    fast = process(process(fast))
+    slow = process(slow)
+    if (fast == 1) return true
+    if (fast == slow) reuturn false
+
+9....9 (10 9s)
+81 * 10 = 810 -> won't overflow 
 */
