4cdabf4e2320c8306f09e56d6dceb2d4861e4f17
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-22 21:16:09 -0500

    Time: 0 ms (100%), Space: 8.1 MB (61.4%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.size() != goal.size()) return false;
+        return (s + s).find(goal) != string::npos;
+    }
+};
+/*
+we can check if s+s contains goal to ignore the rotation effect
+*/
