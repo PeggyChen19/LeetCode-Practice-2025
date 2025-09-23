47ddd3167a3f1cf50db931ff45b16ae85931390a
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-23 00:45:14 -0500

    Time: 19 ms (18.44%), Space: 14 MB (61.41%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int result = nums[0] + nums[1] + nums[2]; // len(nums) >= 3
+        int n = nums.size();
+        int minDiff = INT_MAX;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            int left = i + 1, right = nums.size() - 1;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) continue; // avoid duplication
+            int left = i + 1, right = n - 1;
             while (left < right) {
-                int threeSum = nums[i] + nums[left] + nums[right];
-                if (threeSum == target) {
-                    return target;
-                } else if (threeSum > target) { // make two sum smaller
-                    right--;
-                } else { // make two sum bigger
-                    left++;
+                int sum = nums[i] + nums[left] + nums[right];
+                if (abs(target - sum) < abs(minDiff)) { // find a smaller diff
+                    minDiff = target - sum;
                 }
-                if (abs(threeSum - target) < abs(result - target)) { // find better result
-                    result = threeSum;
+                if (sum == target) return target;
+                else if (sum > target) {
+                    do right--; while (left < right && nums[right] == nums[right + 1]);
+                } else {
+                    do left++; while (left < right && nums[left - 1] == nums[left]);
                 }
             }
         }
-        return result;
+        return target - minDiff;
     }
 };
 /*
-3 Sum closest to the target
-Only have exactly one solution.
----
-1. sort the array: O(nlogn)
-2. choose the first num
-    a. use two pointers find the remaining two nums (make it closest to target)
-Time Complexity: O(n^2)
+maintain a global int minDiff
+do something like find 3 int that sums up to exactly target -> try to be as close to the ans as possible
+
+int minDiff = INT_MAX
+sort(nums)
+for (int i = 0; i < size; i++)
+    fix i as the first element
+    create two pointers: left = i + 1, right = size - 1
+        move left & right to try get closer to the target, update minDiff if found a closer one
+return target - minDiff
 */
