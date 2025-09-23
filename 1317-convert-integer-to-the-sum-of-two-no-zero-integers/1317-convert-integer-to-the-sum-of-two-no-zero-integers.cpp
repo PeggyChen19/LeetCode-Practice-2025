9512e61b462a09137c9148040ce673a29f607e9c
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-22 22:28:40 -0500

    Time: 0 ms (100%), Space: 8.2 MB (61.97%) - LeetHub

--- 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<int> getNoZeroIntegers(int n) {
-        int step = 1;
+        int pow10 = 1;
         int a = 0, b = 0;
         while (n > 0) {
             int digit = n % 10;
             n /= 10;
             if (n == 0 || (digit != 0 && digit != 1)) {
-                a += (digit - 1) * step;
-                b += step;
+                a += (digit - 1) * pow10;
+                b += pow10;
             } else {
                 digit += 10;
-                a += (digit - 5) * step;
-                b += 5 * step;
+                a += (digit - 5) * pow10;
+                b += 5 * pow10;
                 n--;
             }
-            step *= 10;
+            pow10 *= 10;
         }
         return {a, b};
     }
@@ -26,19 +26,19 @@ for each digit, try to find the pair not having 0
 if digit is highest OR (digit != 0 AND digit != 1): easy! {digit - 1, 1}
 else: treat digit as 10 OR 11, borrow carry from higher digit
 
-int step = 1
+int pow10 = 1
 while n > 0
     digit = n % 10
     n /= 10
     if (highest digit || (digit != 0 && digit != 1)) {
-        digitA += (digit - 1) * step
-        digitB += step
+        digitA += (digit - 1) * pow10
+        digitB += pow10
     } else {
         // treat it as 10 or 11, borrow carry from higher digit
         digit += 10
-        digitA = (digit - 5) * step
-        digitB = 5 * step
+        digitA = (digit - 5) * pow10
+        digitB = 5 * pow10
         n--
     }
-    step *= 10
+    pow10 *= 10
 */
