191e7be04e3331332f0e39a5f450e213b99da44f
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-24 01:09:42 -0500

    Time: 0 ms (100%), Space: 52.1 MB (88.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int max_reach = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > max_reach) {
-                return false;
-            }
-            max_reach = max(max_reach, i + nums[i]);
+        int maxReachable = 0, i = 0;
+        while (i <= maxReachable) {
+            maxReachable = max(maxReachable, i + nums[i]);
+            if (maxReachable >= nums.size() - 1) return true;
+            i++;
         }
-        return true;
+        return false;
     }
 };
 /*
-2 3 1 0 4
-2->3->4
-
-int max_reach = 0
-for (int i = 0; i < nums.size(); i++)
-    if (i > max_reach) 
-        return false
-    max_reach = max( max_reach, i + nums[i])
-return true
+Greedy
+while (i <= maxReachable)
+    update maxReachable
+    if maxReachable >= size - 1
+        return true
+return false
 */
