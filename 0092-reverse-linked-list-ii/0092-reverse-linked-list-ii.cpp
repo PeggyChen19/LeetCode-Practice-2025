5d99e29b706ad1ab92f8a92d083678ca94b10909
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-24 01:57:26 -0500

    Time: 0 ms (100%), Space: 11.2 MB (38.11%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if (left == right) { // not need reverse
-            return head;
+        if (left == right) return head;
+        ListNode* dummyHead = new ListNode();
+        dummyHead->next = head;
+        ListNode* leftPre = dummyHead;
+        ListNode* cur = head;
+        for (int i = 1; i < left; i++) {
+            leftPre = cur;
+            cur = cur->next;
         }
-        ListNode* dummy = new ListNode();
-        dummy->next = head;
-        ListNode* pre = dummy;
-        for (int i = 1; i < left; i++) { // cur will stop at left-1
-            pre = pre->next;
+        ListNode* leftStart = cur;
+        ListNode* pre = nullptr;
+        for (int i = 0; i < right - left + 1; i++) { // reverse right - left + 1 times
+            ListNode* realNext = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = realNext;
         }
-        ListNode* cur = pre->next;
-        for (int i = 0; i < (right - left); i++) {
-            ListNode* rangeHead = cur->next;
-            cur->next = rangeHead->next;
-            rangeHead->next = pre->next;
-            pre->next = rangeHead;
-        }
-        return dummy->next;
+        // cur is rightNext
+        // pre is right
+        leftPre->next = pre;
+        leftStart->next = cur;
+        return dummyHead->next;
     }
 };
 /*
-直接用原本的 reverse 會變得很複雜 -> Head Insertion
-Head Insertion: reverse a sublist in a linked list
-1. Find Pointers
-    a. pre: the node before the sublist
-    b. cur: the first node in the sublist
-2. Repeat right - left times:
-    a. find the future new head of the sublist: rangeHead = cur->next
-    b. detach rangeHead: cur->next = rangeHead->next: 
-    c. insert rangeHead right after pre:
-        rangeHead->next = pre->next
-        pre->next = rangeHead    
+l = 2, r = 4
+1 [2 3 4] 5 
+^  l   r  ^
+leftPre   rightNext
+1 [4 3 2] 5
+
+1. find the left, record leftPre
+2. reverse internal until right, record rightNext
+3. leftPre->next = right, left->next = rightNext
 */
