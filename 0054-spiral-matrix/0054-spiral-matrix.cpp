e4c4eabb8f82b67f7a617c99ea1be797c770da22
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-24 00:57:20 -0500

    Time: 0 ms (100%), Space: 9.3 MB (49.79%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,43 +2,42 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> ans;
-        int row_start = 0, row_end = matrix.size() - 1;
-        int col_start = 0, col_end = matrix[0].size() - 1;
-        while (row_start <= row_end && col_start <= col_end) {
-            for (int i = col_start; i <= col_end; i++) { // left to right
-                ans.push_back(matrix[row_start][i]);
+        int top = 0, bottom = matrix.size() - 1;
+        int left = 0, right = matrix[0].size() - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) { // left to right
+                ans.push_back(matrix[top][i]);
             }
-            row_start++;
-            if (row_start > row_end) break;
-            for (int i = row_start; i <= row_end; i++) { // top to down
-                ans.push_back(matrix[i][col_end]);
+            top++;
+            for (int i = top; i <= bottom; i++) { // top to down
+                ans.push_back(matrix[i][right]);
             }
-            col_end--;
-            if (col_end < col_start) break;
-            for (int i = col_end; i >= col_start; i--) { // right to left
-                ans.push_back(matrix[row_end][i]);
+            right--;
+            if (top > bottom) break;
+            for (int i = right; i >= left; i--) { // right to left
+                ans.push_back(matrix[bottom][i]);
             }
-            row_end--;
-            if (row_end < row_start) break;
-            for (int i = row_end; i >= row_start; i--) { // down to top
-                ans.push_back(matrix[i][col_start]);
+            bottom--;
+            if (left > right) break;
+            for (int i = bottom; i >= top; i--) { // down to top
+                ans.push_back(matrix[i][left]);
             }
-            col_start++;
+            left++;
         }
         return ans;
     }
 };
 /*
 boundary: 
-row: 0~m, col: 0~n
+top~bottom: 0~m, left~right: 0~n
 
 moving directions:
 1. left to right
-row.start++
+top++
 2. top to down
-col.end--
+right--
 3. right to left
-row.end--
+bottom--
 4. down to top
-col.start++
+left++
 */
