80e5c81dd5fb20c4ba944d820520c11cffbb32ee
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-25 20:39:56 -0500

    Time: 3 ms (10.8%), Space: 9.1 MB (75.98%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,50 +1,53 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack<int> repeatSt;
-        stack<string> strSt;
-        string curStr = "";
+        return buildString(s, 1);
+    }
+private:
+    string buildString(string s, int k) {
+        string result = "";
         int i = 0;
         while (i < s.size()) {
-            int repeat = 0;
-            while (i < s.size() && s[i] >= '0' && s[i] <= '9') {
-                repeat = repeat * 10 + (s[i] - '0');
+            int k = 0;
+            while (s[i] >= '0' && s[i] <= '9') {
+                k = k * 10 + (s[i] - '0');
                 i++;
             }
-            if (repeat != 0) {
-                repeatSt.push(repeat);
-                strSt.push(curStr);
-                curStr = "";
-            } else if (s[i] == ']'){
-                string repeated = "";
-                for (int j = 0; j < repeatSt.top(); j++) {
-                    repeated += curStr;
+            if (k > 0) { // valid k
+                int start = i + 1; // skip [
+                int unmatched = 0;
+                do {
+                    if (s[i] == '[') unmatched++;
+                    if (s[i] == ']') unmatched--;
+                    i++;
+                } while (unmatched);
+                int end = i - 2; // i will stop at the first element after ], go back to the end of sub string
+                string repeated = buildString(s.substr(start, (end - start + 1)), k);
+                for (int j = 0; j < k; j++) {
+                    result += repeated;
                 }
-                curStr = strSt.top() + repeated;
-                repeatSt.pop();
-                strSt.pop();
             } else {
-                curStr += s[i];
+                result += s[i];
+                i++;
             }
-            i++;
         }
-        return curStr;
+        return result;
     }
 };
 /*
-digits are only used for repeating
-output not exceed 10^5 -> no overflow
-3[a2[bc]]
-
-Nested structure: recursion / stack
-Encouter k[
-1. store k & preStr
-2. create curStr (enter a new layer to update curStr)
-
-Encounter ]
-1. repeat curStr with k times
-2. preStr += curStr
-3. curStr = preStr
-
-We need two stack to record the multiple previous k and preStr
+nested structure -> recursion
+0 1 2 3 4 5 6
+4 [ a b c ]
+    s       i
+string buildString(string s, int k)
+    string result = ""
+    i = 0
+    while (i < s.size())
+        if s[i] is digit
+            // get the repeated_sub_string (without [])
+            result += buildString(repeated_sub_string, digit)
+        else
+            result += s[i]
+        i++
+    return result
 */
