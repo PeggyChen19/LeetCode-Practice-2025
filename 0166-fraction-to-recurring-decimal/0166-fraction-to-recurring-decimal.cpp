504f25d5cb92425b3d03d1c870bd4bf5b734153a
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-27 18:18:24 -0500

    Time: 0 ms (100%), Space: 8.7 MB (83.16%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
@@ -1,41 +1,41 @@
-/*
-detect negative
-no remainder: return ans
-remainder:
-1. record the remainder & posistion 
-2. detect repeat remainder
-3. insert
-4. update remainder
-*/
-
 class Solution {
 public:
     string fractionToDecimal(int numerator, int denominator) {
-        if(numerator == 0) return "0";
-        string ans = "";
-        if(numerator < 0 ^ denominator < 0) ans += '-';
-        long n = abs(numerator);
-        long d = abs(denominator);
-        
-        ans += to_string(n / d);
-        long r = n % d;
-        if(r == 0) return ans;
-        else ans += '.';
+        if (numerator == 0) return "0";
+        if (denominator == 0) return "invalid";
+        bool negative = ((numerator < 0) ^ (denominator < 0)) ? true : false;
+        long long n = llabs(numerator);
+        long long d = llabs(denominator);
 
-        unordered_map<long, long> pos;
-        while(r != 0){
-            if(!pos.count(r)){
-                pos[r] = ans.size();
+        long long integer = n / d;
+        long long remainder = n % d;
+        string result = negative ? "-" : "";
+        result += to_string(integer);
+        if (remainder == 0) return result;
+        else result.push_back('.');
+
+        unordered_map<int, int> mapping; // remainder: index
+        while (remainder != 0) {
+            if (mapping.count(remainder)) {
+                int pos = mapping[remainder];
+                result.insert(pos, 1,'(');
+                result.push_back(')');
+                return result;
             }
-            else{
-                ans.insert(pos[r], "(");
-                ans += ')';
-                break;
-            }
-            r *= 10;
-            ans += to_string(r / d);
-            r %= d;
+            mapping[remainder] = result.size();
+            remainder *= 10;
+            int digit = remainder / d;
+            remainder %= d;
+            result.push_back(char('0' + digit));
         }
-        return ans;
+        return result;
     }
 };
+/*
+"remainder" makes the float digit repeat, not the digit itself
+use unordered_map to record {remainder, index} pair, when found the same remainder -> insert '('
+
+4/333
+integer = 0
+remainder = 40
+*/
