c5a8d775ce584d550ef2345164466eeeed4c2acf
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-27 11:34:23 -0500

    Time: 2 ms (69.48%), Space: 8.9 MB (96.24%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,37 +1,53 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        int len1 = num1.size(), len2 = num2.size();
-        vector<int> result(len1 + len2, 0);
-        for (int i = len1-1; i >= 0; i--) {
-            for (int j = len2-1; j >= 0; j--) {
-                result[i+j+1] += (num1[i] - '0') * (num2[j] - '0');
+        if (num1 == "0" || num2 == "0") return "0";
+        string result = "";
+        int size1 = num1.size(), size2 = num2.size();
+        for (int i = 0; i < size1 + size2; i++) {
+            result.push_back('0'); // init result
+        }
+        for (int i = size1 - 1; i >= 0; i--) {
+            int a = num1[i] - '0';
+            for (int j = size2 - 1; j >= 0; j--) {
+                int b = num2[j] - '0';
+                int sum = (result[i + j + 1] - '0') + a * b; 
+                result[i + j + 1] = char('0' + (sum % 10));
+                result[i + j] = char(result[i + j] + sum / 10); // it might > '9', but it will fix (mod) later
             }
         }
-        for (int i = len1+len2-1; i > 0; i--) {
-            result[i-1] += (result[i] / 10);
-            result[i] %= 10;
+        int start = 0;
+        while (start < result.size() && result[start] == '0') {
+            start++;
         }
-        string ans = "";
-        for (int i = 0; i < len1+len2; i++) {
-            if (ans == "" && result[i] == 0) { // ignore leading zero
-                continue;
-            }
-            ans += to_string(result[i]);
-        }
-        return ans == "" ? "0" : ans;
+        return result.substr(start);
     }
 };
 /*
-at most len1 + len2 digits
-for nums[i] * nums[j] -> store in i+j+1 (because it's 0 indexed)
-    0 1 2
-    1 2 3
-      4 5
-----------
-    * * *
-  * * *  
-----------
-0 1 2 3 4
-Create a result vector and then handle the carries after calculating all digits
+if num1 or num2 == "0" -> return "0"
+rerserve result string size (size1 + size2) with all '0'
+carry = 0
+for i = size1 - 1; i >= 0; i--
+    for j = size2 - 1; j >= 0; j--
+        result[i + j + 1] += carry + (nums1[i] * nums2[j] % 10)
+        update carry
+remove the leading zero 
+
+      0 1 2 (index)
+      1 2 3
+      1 2 3
+  ----------
+      3 6 9
+    3 6 9
+  ----------
+x x 4 0 5 9
+0 1 2 3 4 5 (index)
+
+     9 9
+     9 9
+   -----
+   8 9 1
+ 8 9 1
+--------
+
 */
