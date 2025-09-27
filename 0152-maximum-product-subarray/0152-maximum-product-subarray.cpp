ff298b7cf6ca76c9ae0cd3a95c147e5c2ae0fab0
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-27 14:54:57 -0500

    Time: 0 ms (100%), Space: 17.6 MB (85.54%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,30 +2,19 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         if (nums.empty()) return 0;
-        int global_max = nums[0], negative_min = min(0, nums[0]), positive_max = max(0, nums[0]);
+        int global_max = nums[0], cur_min = nums[0], cur_max =nums[0];
         for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] < 0) {
-                int original_positive_max = positive_max;
-                positive_max = negative_min * nums[i];
-                negative_min = min(original_positive_max * nums[i], nums[i]);
-            } else {
-                negative_min = negative_min * nums[i];
-                positive_max = max(positive_max * nums[i], nums[i]);
-            }
-            global_max = max(global_max, positive_max);
+            int from_max = cur_max * nums[i];
+            int from_min = cur_min * nums[i];
+            cur_min = min(nums[i], min(from_max, from_min));
+            cur_max = max(nums[i], max(from_max, from_min));
+            global_max = max(global_max, cur_max);
         }
         return global_max;
     }
 };
 
 /*
-abs(bigProduct) has potential -> record it
-
-negative_min -> become bigger when encounter negative
-positive_max -> become bigger when encounter positive
-
+abs(bigProduct) has potential -> record both curMin & curMax
 global_max -> the result we return
-negative_min[i]: the smallest negative product subarray ends with i
-positive_max[i]: the biggest positive product subarray ends with i
-we only need the previous negative_min & positive_max
 */
