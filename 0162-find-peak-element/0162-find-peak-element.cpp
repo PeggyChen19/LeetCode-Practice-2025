37ca1333f0fd4b44af900559460e3ae34306e255
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-27 15:37:13 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n =  nums.size();
+        int n = nums.size();
         int left = 0, right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == n - 1 || nums[mid] > nums[mid + 1])) {
-                return mid; // peak
-            } else if (mid == 0 || nums[mid - 1] < nums[mid]) { // go right
+            if ( (mid == 0 || nums[mid - 1] < nums[mid]) && (mid == n - 1 || nums[mid] > nums[mid + 1]) { // peak
+                return mid;
+            } else if (mid == 0 || nums[mid - 1] < nums[mid]) { // increasing -> go right
                 left = mid + 1;
-            } else { // go left
+            } else { // decreasing -> go left
                 right = mid - 1;
             }
         }
@@ -17,11 +17,13 @@ public:
     }
 };
 /*
-time: O(logn)
-binary search
-for a peak: (up: up, d: down)
-u u * d d d u u u * d d d
-if (nums[i] > nums[i-1] && nums[i] > nums[i + 1]) return peak
-else if (nums[i - 1] < nums[i]) up -> go right
-else if (nums[i - 1] > nums[i]) down -> go left
+Time limit: O(logn) -> shrink the search range based on some condition -> binary search
+set nums[-1] = nums[n] = -infinite
+for each round, find mid, mid - 1, mid + 1
+if mid is peak
+    return mid
+elif mid is in increasing order
+    find peak from right part
+else
+    find peak from left part
 */
