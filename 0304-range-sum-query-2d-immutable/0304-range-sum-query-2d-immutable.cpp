edcc5acca89481b8993cfada79a17073811f7ca6
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-28 14:50:45 -0500

    Time: 45 ms (18.17%), Space: 153.9 MB (5.31%) - LeetHub

---
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,39 @@
+class NumMatrix {
+private:
+    vector<vector<int>> regionSum;
+
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix.size(); i++) {
+            regionSum.push_back({});
+            int prefixSum = 0;
+            for (int j = 0; j < matrix[0].size(); j++) {
+                prefixSum += matrix[i][j];
+                if (i == 0) {
+                    regionSum[i].push_back(prefixSum);
+                } else {
+                    regionSum[i].push_back(regionSum[i - 1][j] + prefixSum);
+                }
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int top = (row1 - 1 < 0) ? 0 : regionSum[row1 - 1][col2];
+        int left = (col1 - 1 < 0) ? 0 : regionSum[row2][col1 - 1];
+        int topLeft = ((row1 - 1 < 0) || (col1 - 1 < 0)) ? 0 : regionSum[row1 - 1][col1 - 1];
+        return regionSum[row2][col2] - top - left + topLeft;
+    }
+};
+
+/*
+Time Complexity -> O(1)
+For one row -> intervalSum[i, j] = prefixSum[j] - prefixSum[i]
+2 dimension prefix sum? (topLeft - bottomRight)?
+
+1. record each row prefixSum = prefixSum[i-1] + nums[i] -> O(n*n)
+2. record prefix regionSum[i][j] = regionSum[i-1][j] + prefixSum[j] -> O(n*m)
+
+result[row1][col1][row2][col2]
+= regionSum[row2][col2] - regionSum[row1 - 1][col2] - regionSum[row2][col1 - 1] + regionSum[row1 - 1][col1 - 1]
+*/
