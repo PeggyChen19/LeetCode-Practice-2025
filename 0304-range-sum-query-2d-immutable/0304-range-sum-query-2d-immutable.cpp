0d342ed7091bdf3642b3c975273f08f3ada17c28
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-09-28 14:54:39 -0500

    Time: 29 ms (41.01%), Space: 149.2 MB (29.63%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -6,9 +6,9 @@ public:
     NumMatrix(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
         regionSum = vector<vector<int>>(m, vector<int>(n, 0));
-        for (int i = 0; i < matrix.size(); i++) {
+        for (int i = 0; i < m; i++) {
             int prefixSum = 0;
-            for (int j = 0; j < matrix[0].size(); j++) {
+            for (int j = 0; j < n; j++) {
                 prefixSum += matrix[i][j];
                 if (i == 0) {
                     regionSum[i][j]= prefixSum;
