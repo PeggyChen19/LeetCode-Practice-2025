df4a10b09419c915deb9b79978f00955c84d28a2
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-10-02 21:16:27 -0500

    Time: 3 ms (61.2%), Space: 23.6 MB (20.95%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,49 +1,35 @@
-class Element {
-public:
-    int val, min;
-    Element* next;
-    Element(int val, int min, Element* next): val(val), min(min), next(next) {}
-};
-
 class MinStack {
 private:
-    Element* top_element;
+    stack<int> st;
+    stack<int> decreasing;
+
 public:
-    MinStack() {
-        top_element = new Element(0, INT_MAX, nullptr);
-    }
+    MinStack() {}
     
     void push(int val) {
-        Element* new_element = new Element(val, min(val, top_element->min), top_element);        
-        top_element = new_element;
+        st.push(val);
+        if (decreasing.empty() || val <= decreasing.top()) {
+            decreasing.push(val);
+        }
     }
     
     void pop() {
-        Element* to_remove = top_element;
-        top_element = top_element->next;
-        delete to_remove;
+        if (st.top() == decreasing.top()) {
+            decreasing.pop();
+        }
+        st.pop();
     }
     
     int top() {
-        return top_element->val;
+        return st.top();
     }
     
     int getMin() {
-        return top_element->min;
+        return decreasing.top();
     }
 };
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- 
-push, pop, top -> normal stack
-min -> for each element, we can record the min from bottom~element
-    -> when we pop, the min record of remaining elements won't affect
-
-we can use build-in stack or create our own via linked list
+/*
+1. normal stack: 2 3 1 4 5 6 0
+2. decreasing stack: 2 1 0
 */
