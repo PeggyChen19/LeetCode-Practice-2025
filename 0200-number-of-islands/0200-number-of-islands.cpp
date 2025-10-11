91e18db8936cb46ddd9ac06e6abca03d21b4c765
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-10-11 14:48:19 -0500

    Time: 23 ms (83.69%), Space: 18.7 MB (40.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,40 +1,72 @@
+class UnionFind {
+public:
+    vector<int> parent; // turn 2-dimension vector to 1 by i*col_size+j
+    vector<int> rank;
+    int count;
+
+    UnionFind(int size) {
+        rank.resize(size, 0);
+        for (int i = 0; i < size; i++) {
+            parent.push_back(i); // the parent is itself in the beginning
+        }
+    }
+
+    int Find(int n) {
+        if (parent[n] != n) {
+            parent[n] = Find(parent[n]); // recursive until the root
+        }
+        return parent[n];
+    }
+
+    void Union(int a, int b) {
+        int root_a = Find(a);
+        int root_b = Find(b);
+        if (root_a == root_b) { // in the same set already
+            return;
+        }
+        if (rank[root_a] > rank[root_b]) {
+            parent[root_b] = root_a;
+        } else if (rank[root_b] > rank[root_a]) {
+            parent[root_a] = root_b;
+        } else {
+            parent[root_a] = root_b;
+            rank[root_b]++;
+        }
+        count--;
+    }
+
+};
+
 class Solution {
+private:
+    const vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int result = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
+        int m = grid.size();
+        int n = grid[0].size();
+        UnionFind uf(m * n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                   bfs(grid, i, j);
-                   result++;
+                    uf.count++;
                 }
             }
         }
-        return result;
-    }
-private:
-    vector<pair<int, int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    
-    void bfs(vector<vector<char>>& grid, int i, int j) {
-        queue<pair<int, int>> q;
-        q.push({i, j});
-        grid[i][j] = '0'; 
-        while (!q.empty()) {
-            auto [x, y] = q.front();
-            q.pop();
-            for (auto& [dx, dy] : directions) {
-                int a = x + dx;
-                int b = y + dy;
-                if (a >= 0 && a < grid.size() && b >= 0 && b < grid[0].size() && grid[a][b] == '1') {
-                    q.push({a, b});
-                    grid[a][b] = '0';
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    // grid[i][j] = '0';
+                    for (auto [dx, dy] : directions) {
+                        int x = i + dx;
+                        int y = j + dy;
+                        if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == '1') {
+                            uf.Union((i * n + j), (x * n + y));
+                        }
+                    }
                 }
             }
         }
+        return uf.count;
     }
 };
-/*
-DFS / BFS
-After visiting -> set to 0 (water)
-go through the matrix, explore every island, return the count
-*/
