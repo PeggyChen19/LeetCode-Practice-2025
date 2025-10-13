d9fc6c20b2eec2265337e7c26e5cd119c511a99e
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2025-10-13 16:37:26 -0500

    Time: 173 ms (6.38%), Space: 272.1 MB (5%) - LeetHub

--- 3484-design-spreadsheet/3484-design-spreadsheet.cpp
+++ 3484-design-spreadsheet/3484-design-spreadsheet.cpp
@@ -20,37 +20,43 @@ public:
     }
     
     int getValue(string formula) {
-        formula = formula.substr(1);
-        istringstream iss(formula);
-        string element;
-        int result = 0;
-        while (getline(iss, element, '+')) {
-            if (element[0] >= 'A' && element[0] <= 'Z') { // cell
-                auto [i, j] = getCell(element);
-                result += sheet[i][j];
-            } else { // non-negative integers
-                int i = 0;
-                int num = 0;
-                while (i < element.size()) {
-                    num = num * 10 + element[i] - '0';
-                    i++;    
-                }
-                result += num;
-            }
-        }
-        return result;
+        int i = 1; // skip =
+        int x = parseElement(formula, i);
+        i++; // skip +
+        int y = parseElement(formula, i);
+        return x + y;
     }
 
 private:
-    pair<int, int> getCell(string cell) {
-        char alphabet = cell[0];
+    pair<int, int> getCell(string& cell) {
+        char col = cell[0] - 'A';
         int i = 1;
         int row = 0;
         while (i < cell.size()) {
             row = row * 10 + cell[i] - '0';
             i++;
         }
-        return {row - 1, alphabet - 'A'}; // row - 1 because of 1-indexed
+        return {row - 1, col}; // row - 1 because of 1-indexed
+    }
+
+    int parseElement(string& formula, int& i) {
+        if (formula[i] >= 'A' && formula[i] <= 'Z') { // cell
+            int col = formula[i] - 'A';
+            i++;
+            int row = 0;
+            while (i < formula.size() && isdigit(formula[i])) {
+                row = row * 10 + formula[i] - '0';
+                i++;
+            }
+            return sheet[row - 1][col];
+        } else { // non-negative integers
+            int num = 0;
+            while (i < formula.size() && isdigit(formula[i])) {
+                num = num * 10 + formula[i] - '0';
+                i++;
+            }
+            return num;
+        }
     }
 };
 
