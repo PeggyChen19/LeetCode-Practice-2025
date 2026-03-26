6afa80ceaf5206cc3b780b4502e6f38789acd825
Peggy (Chun-Pei) Chen <86610644+PeggyChen19@users.noreply.github.com>
2026-03-26 02:41:35 -0500

    Time: 11 ms (47.13%), Space: 11 MB (99.7%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        unordered_map<char, int> map; // our need for this char
-        int unmatched = t.size();
-        int left = 0, right = 0, head = 0, minL = s.size()+1;
-        for (char c : t) {
-            map[c]++;
+    string minWindow(string& s, string& t) {
+        unordered_map<char, int>freq;
+        int minLen = INT_MAX;
+        int start = -1;
+
+        for(char c : t){
+            freq[c]++;
         }
-        while (right < s.size()) {
-            if (map[s[right]] > 0) { // s[right] matches unmatched element in t
-                unmatched--;
+
+        int require = t.size();
+        int left = 0;
+        int right = 0;
+        while(right < s.size()){
+            char c = s[right];
+
+            if(freq.count(c)){
+                if(freq[c] > 0) require--;
+                freq[c]--;
             }
-            map[s[right]]--; // our need reduce because we add s[right]
-            while (unmatched == 0) { // satisfy the condition -> keep finding the min
-                int cur_len = right - left + 1;
-                if (cur_len < minL) { // update min
-                    head = left;
-                    minL = cur_len;
+
+            while(require == 0){
+                int len = right - left + 1;
+                if(require == 0 && len < minLen){
+                    minLen = len;
+                    start = left;
                 }
-                map[s[left]]++; // our need increase because we remove s[left]
-                if (map[s[left]] > 0) { // there is more need than we have
-                    unmatched++;
+                char l = s[left];
+                if(freq.count(l)){
+                    if(freq[l] >= 0) require++;
+                    freq[l]++;
                 }
                 left++;
             }
+
             right++;
         }
-        return (minL == s.size()+1) ? "" : s.substr(head, minL);
+        return (minLen != INT_MAX) ? s.substr(start, minLen) : "";
     }
 };
-/*
-Sliding window:
-create a var to record the min
-set left and right at the first element
-while right didn't reach the end
-    put right into window
-    while satisfy the conditin 
-        update result(min)
-        shrink left
-    expand right
 
-How to check it is valid or not
-unordered_map -> char_t : counts
-total -> the number of unmatch
-*/
+
